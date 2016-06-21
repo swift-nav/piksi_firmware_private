gsmcmullin/piksi_firmware

Add L2C tracking

src/board/v3/nap/track_channel.c
@@ -50,45 +50,177 @@ static struct {
   u32 len;
 } nap_ch_state[NAP_MAX_N_TRACK_CHANNELS];
 
-static u32 calc_length_samples(u8 codes, s32 cp_start, u32 cp_rate)
+/** Compute the correlation length in the units of sampling frequency samples.
+ * \param chips_to_correlate The number of chips to correlate over.
+ * \param cp_start Initial code phase in NAP units.
+ * \param cp_rate Code phase rate.
+ * \return The correlation length in NAP units
+ */
+static u32 calc_length_samples(u32 chips_to_correlate, s32 cp_start, u32 cp_rate)
 {
-  u16 chips = codes * 1023;
-  u64 cp_units = (1ULL << NAP_TRACK_CODE_PHASE_FRACTIONAL_WIDTH) * chips
-                 - cp_start;
+  u64 cp_units = (1ULL << NAP_TRACK_CODE_PHASE_FRACTIONAL_WIDTH) *
+                 chips_to_correlate - cp_start;
   u32 samples = cp_units / cp_rate;
   return samples;
 }
 
-void nap_track_init(u8 channel, gnss_signal_t sid, u32 ref_timing_count,
-                   float carrier_freq, float code_phase)
+/** Return the init value of NAP G1 pseudorandom generator.
+ * \param sid Signal ID
+ * \return NAP G1 initial value
+ */
+u32 sid_to_init_g1(gnss_signal_t sid)
 {
+  u32 ret = ~0;
+  /* The L2C G1 init values are taken from IS-GPS-200H Table 3-IIa
+     "Code phase assignments (IIR-M, IIF, and subsequent blocks only)"
+     For example, PRN 1 has G1 init value 0742417664.
+   */
+  u32 gps_l2cm_prns_init_values[] = {
+    /* 0  */ 0742417664,
+    /* 1  */ 0756014035,
+    /* 2  */ 0002747144,
+    /* 3  */ 0066265724,
+    /* 4  */ 0601403471,
+    /* 5  */ 0703232733,
+    /* 6  */ 0124510070,
+    /* 7  */ 0617316361,
+    /* 8  */ 0047541621,
+    /* 9  */ 0733031046,
+    /* 10 */ 0713512145,
+    /* 11 */ 0024437606,
+    /* 12 */ 0021264003,
+    /* 13 */ 0230655351,
+    /* 14 */ 0001314400,
+    /* 15 */ 0222021506,
+    /* 16 */ 0540264026,
+    /* 17 */ 0205521705,
+    /* 18 */ 0064022144,
+    /* 19 */ 0120161274,
+    /* 20 */ 0044023533,
+    /* 21 */ 0724744327,
+    /* 22 */ 0045743577,
+    /* 23 */ 0741201660,
+    /* 24 */ 0700274134,
+    /* 25 */ 0010247261,
+    /* 26 */ 0713433445,
+    /* 27 */ 0737324162,
+    /* 28 */ 0311627434,
+    /* 29 */ 0710452007,
+    /* 30 */ 0722462133,
+    /* 31 */ 0050172213,
+    /* 32 */ 0500653703,
+    /* 33 */ 0755077436,
+    /* 34 */ 0136717361,
+    /* 35 */ 0756675453,
+    /* 36 */ 0435506112
+  };
+
+  switch (sid.code) {
+  case CODE_GPS_L2CM:
+    ret = gps_l2cm_prns_init_values[sid.sat - 1] & 0x7FFFFFF;
+    break;
+  case CODE_GPS_L1CA:
+    ret = 0x3ff;
+    break;
+  default:
+    assert(0);
+    break;
+  }
+
+  return ret;
+}
+
+/** Looks-up RF frontend channel for the given signal ID.
+ * \param sid Signal ID.
+ * \return RF front-end channel number.
+ */
+u8 sid_to_rf_frontend_channel(gnss_signal_t sid)
+{
+  u8 ret = ~0;
+  switch (sid.code) {
+  case CODE_GPS_L1CA:
+  case CODE_SBAS_L1CA:
+    ret = NAP_RF_FRONTEND_CHANNEL_1;
+    break;
+  case CODE_GPS_L2CM:
+    ret = NAP_RF_FRONTEND_CHANNEL_4;
+    break;
+  default:
+    assert(0);
+    break;
+  }
+  return ret;
+}
+
+/** Looks-up NAP constellation and band code for the given signal ID.
+ * \param sid Signal ID.
+ * \return NAP constallation and band code.
+ */
+u8 sid_to_nap_code(gnss_signal_t sid)
+{
+  u8 ret = ~0;
+  switch (sid.code) {
+  case CODE_GPS_L1CA:
+  case CODE_SBAS_L1CA:
+    ret = NAP_CONSTELLATION_BAND_1;
+    break;
+  case CODE_GPS_L2CM:
+    ret = NAP_CONSTELLATION_BAND_2;
+    break;
+  default:
+    assert(0);
+    break;
+  }
+  return ret;
+}
+
+void nap_track_init(u8 channel, gnss_signal_t sid, u32 ref_timing_count,
+                   float carrier_freq, float code_phase, u32 chips_to_correlate)
+{
+  u16 control;
   u32 now = NAP->TIMING_COUNT;
   u32 track_count = now + 200000;
   double cp = propagate_code_phase(code_phase, carrier_freq,
-                                  track_count - ref_timing_count);
+                                  track_count - ref_timing_count, sid.code);
 
   /* Contrive for the timing strobe to occur at or close to a
    * PRN edge (code phase = 0) */
-  track_count += (SAMPLE_FREQ/GPS_CA_CHIPPING_RATE) * (1023.0-cp) *
-                 (1.0 + carrier_freq / GPS_L1_HZ);
+  track_count += (SAMPLE_FREQ / code_to_chip_rate(sid.code)) *
+                 (code_to_chip_num(sid.code) - cp) *
+                 (1.0 + carrier_freq / code_to_carr_freq(sid.code));
 
   u8 prn = sid.sat - 1;
-  NAP->TRK_CH[channel].CONTROL = prn << NAP_TRK_CONTROL_SAT_Pos;
+  /* PRN code */
+  control = (prn << NAP_TRK_CONTROL_SAT_Pos) & NAP_TRK_CONTROL_SAT_Msk;
+  /* RF frontend channel */
+  control |= (sid_to_rf_frontend_channel(sid) << NAP_TRK_CONTROL_RF_FE_Pos) & \
+             NAP_TRK_CONTROL_RF_FE_Msk;
+  /* Constellation and band for tracking */
+  control |= (sid_to_nap_code(sid) << NAP_TRK_CONTROL_CODE_Pos) & \
+             NAP_TRK_CONTROL_CODE_Msk;
+  /* We are not utilizing multiple signals within one RF channel at the moment.
+     Therefore, RF_FE_CH is 0 and below statement in a NOP. */
+  control |= (0 << NAP_TRK_CONTROL_RF_FE_CH_Pos) & NAP_TRK_CONTROL_RF_FE_CH_Msk;
+
+  NAP->TRK_CH[channel].CONTROL = control;
   /* We always start at zero code phase */
   NAP->TRK_CH[channel].CODE_INIT_INT = 0;
   NAP->TRK_CH[channel].CODE_INIT_FRAC = 0;
-  NAP->TRK_CH[channel].CODE_INIT_G1 = 0x3ff;
+  NAP->TRK_CH[channel].CODE_INIT_G1 = sid_to_init_g1(sid);
   NAP->TRK_CH[channel].CODE_INIT_G2 = 0x3ff;
 
   NAP->TRK_CH[channel].SPACING = (SPACING_HALF_CHIP << 16) | SPACING_HALF_CHIP;
 
-  u32 cp_rate = (1.0 + carrier_freq/GPS_L1_HZ) * GPS_CA_CHIPPING_RATE *
+  u32 cp_rate = (1.0 + carrier_freq / code_to_carr_freq(sid.code)) *
+                code_to_chip_rate(sid.code) *
                 NAP_TRACK_CODE_PHASE_RATE_UNITS_PER_HZ;
 
   NAP->TRK_CH[channel].CARR_PINC = -carrier_freq *
                                    NAP_TRACK_CARRIER_FREQ_UNITS_PER_HZ;
   NAP->TRK_CH[channel].CODE_PINC = cp_rate;
-  nap_ch_state[channel].len = NAP->TRK_CH[channel].LENGTH = calc_length_samples(1, 0, cp_rate)+1;
+
+  u32 length = calc_length_samples(chips_to_correlate, 0, cp_rate) + 1;
+  nap_ch_state[channel].len = NAP->TRK_CH[channel].LENGTH = length;
   nap_ch_state[channel].code_phase = (NAP->TRK_CH[channel].LENGTH) * cp_rate;
   NAP->TRK_CONTROL |= (1 << channel); /* Set to start on the timing strobe */
 
@@ -99,7 +231,7 @@ void nap_track_init(u8 channel, gnss_signal_t sid, u32 ref_timing_count,
 }
 
 void nap_track_update(u8 channel, double carrier_freq,
-                      double code_phase_rate, u8 rollover_count,
+                      double code_phase_rate, u32 chips_to_correlate,
                       u8 corr_spacing)
 {
   (void)corr_spacing; /* This is always written as 0 now... */
@@ -108,9 +240,10 @@ void nap_track_update(u8 channel, double carrier_freq,
   NAP->TRK_CH[channel].CARR_PINC = -carrier_freq *
                                    NAP_TRACK_CARRIER_FREQ_UNITS_PER_HZ;
   NAP->TRK_CH[channel].CODE_PINC = cp_rate_fp;
-  NAP->TRK_CH[channel].LENGTH = calc_length_samples(rollover_count + 1,
-                                    nap_ch_state[channel].code_phase,
-                                    cp_rate_fp);
+  u32 length = calc_length_samples(chips_to_correlate,
+                                   nap_ch_state[channel].code_phase,
+                                   cp_rate_fp);
+  NAP->TRK_CH[channel].LENGTH = length;
 
   volatile u16 cp_int = NAP->TRK_CH[channel].CODE_PHASE_INT;
   if ((cp_int != 0x3fe)) /* Sanity check, we should be just before rollover */

src/simulator.c
@@ -348,7 +348,8 @@ void populate_nav_meas(navigation_measurement_t *nav_meas, double dist, double e
   nav_meas->raw_pseudorange += rand_gaussian(sim_settings.pseudorange_sigma *
                                              sim_settings.pseudorange_sigma);
 
-  nav_meas->carrier_phase =    dist / (GPS_C / GPS_L1_HZ);
+  nav_meas->carrier_phase =     dist / (GPS_C /
+            code_to_carr_freq(simulation_almanacs[almanac_i].sid.code));
   nav_meas->carrier_phase +=   simulation_fake_carrier_bias[almanac_i];
   nav_meas->carrier_phase +=   rand_gaussian(sim_settings.phase_sigma *
                                              sim_settings.phase_sigma);

swift-nav/piksi_firmware_private

src/board/v3/nap/track_channel.c
@@ -54,40 +54,170 @@ static struct nap_ch_state {
   u32 code_phase;   /**< Fractional part of code phase. */
 } nap_ch_state[NAP_MAX_N_TRACK_CHANNELS];
 
-static u32 calc_length_samples(u8 codes, u32 cp_start_frac_units,
+/** Compute the correlation length in the units of sampling frequency samples.
+ * \param chips_to_correlate The number of chips to correlate over.
+ * \param cp_start_frac_units Initial code phase in NAP units.
+ * \param cp_rate_units Code phase rate.
+ * \return The correlation length in NAP units
+ */
+static u32 calc_length_samples(u32 chips_to_correlate, u32 cp_start_frac_units,
                                u32 cp_rate_units)
 {
-  u64 cp_end_units = codes * 1023 * NAP_TRACK_CODE_PHASE_UNITS_PER_CHIP;
+  u64 cp_end_units = chips_to_correlate * NAP_TRACK_CODE_PHASE_UNITS_PER_CHIP;
   /* cp_start_frac_units is reinterpreted as a signed value. This works
    * because NAP_TRACK_CODE_PHASE_FRACTIONAL_WIDTH is equal to 32 */
   u64 cp_units = cp_end_units - (s32)cp_start_frac_units;
   u32 samples = cp_units / cp_rate_units;
+
   return samples;
 }
 
-void nap_track_init(u8 channel, gnss_signal_t sid, u32 ref_timing_count,
-                    float carrier_freq, float code_phase)
+/** Return the init value of NAP G1 pseudorandom generator.
+ * \param sid Signal ID
+ * \return NAP G1 initial value
+ */
+u32 sid_to_init_g1(gnss_signal_t sid)
 {
-  assert(sid.code == CODE_GPS_L1CA);
+  u32 ret = ~0;
+  /* The L2C G1 init values are taken from IS-GPS-200H Table 3-IIa
+     "Code phase assignments (IIR-M, IIF, and subsequent blocks only)"
+     For example, PRN 1 has G1 init value 0742417664.
+   */
+  u32 gps_l2cm_prns_init_values[] = {
+    /* 0  */ 0742417664,
+    /* 1  */ 0756014035,
+    /* 2  */ 0002747144,
+    /* 3  */ 0066265724,
+    /* 4  */ 0601403471,
+    /* 5  */ 0703232733,
+    /* 6  */ 0124510070,
+    /* 7  */ 0617316361,
+    /* 8  */ 0047541621,
+    /* 9  */ 0733031046,
+    /* 10 */ 0713512145,
+    /* 11 */ 0024437606,
+    /* 12 */ 0021264003,
+    /* 13 */ 0230655351,
+    /* 14 */ 0001314400,
+    /* 15 */ 0222021506,
+    /* 16 */ 0540264026,
+    /* 17 */ 0205521705,
+    /* 18 */ 0064022144,
+    /* 19 */ 0120161274,
+    /* 20 */ 0044023533,
+    /* 21 */ 0724744327,
+    /* 22 */ 0045743577,
+    /* 23 */ 0741201660,
+    /* 24 */ 0700274134,
+    /* 25 */ 0010247261,
+    /* 26 */ 0713433445,
+    /* 27 */ 0737324162,
+    /* 28 */ 0311627434,
+    /* 29 */ 0710452007,
+    /* 30 */ 0722462133,
+    /* 31 */ 0050172213,
+    /* 32 */ 0500653703,
+    /* 33 */ 0755077436,
+    /* 34 */ 0136717361,
+    /* 35 */ 0756675453,
+    /* 36 */ 0435506112
+  };
+
+  switch (sid.code) {
+  case CODE_GPS_L2CM:
+    ret = gps_l2cm_prns_init_values[sid.sat - 1] & 0x7FFFFFF;
+    break;
+  case CODE_GPS_L1CA:
+    ret = 0x3ff;
+    break;
+  default:
+    assert(0);
+    break;
+  }
+
+  return ret;
+}
+
+/** Looks-up RF frontend channel for the given signal ID.
+ * \param sid Signal ID.
+ * \return RF front-end channel number.
+ */
+u8 sid_to_rf_frontend_channel(gnss_signal_t sid)
+{
+  u8 ret = ~0;
+  switch (sid.code) {
+  case CODE_GPS_L1CA:
+  case CODE_SBAS_L1CA:
+    ret = NAP_RF_FRONTEND_CHANNEL_1;
+    break;
+  case CODE_GPS_L2CM:
+    ret = NAP_RF_FRONTEND_CHANNEL_4;
+    break;
+  default:
+    assert(0);
+    break;
+  }
+  return ret;
+}
+
+/** Looks-up NAP constellation and band code for the given signal ID.
+ * \param sid Signal ID.
+ * \return NAP constallation and band code.
+ */
+u8 sid_to_nap_code(gnss_signal_t sid)
+{
+  u8 ret = ~0;
+  switch (sid.code) {
+  case CODE_GPS_L1CA:
+  case CODE_SBAS_L1CA:
+    ret = NAP_CONSTELLATION_BAND_1;
+    break;
+  case CODE_GPS_L2CM:
+    ret = NAP_CONSTELLATION_BAND_2;
+    break;
+  default:
+    assert(0);
+    break;
+  }
+  return ret;
+}
+
+void nap_track_init(u8 channel, gnss_signal_t sid, u32 ref_timing_count,
+                   float carrier_freq, float code_phase, u32 chips_to_correlate)
+{
+  assert((sid.code == CODE_GPS_L1CA) || (sid.code == CODE_GPS_L2CM));
 
   nap_trk_regs_t *t = &NAP->TRK_CH[channel];
   struct nap_ch_state *s = &nap_ch_state[channel];
   memset(s, 0, sizeof(*s));
 
+  u16 control;
   u8 prn = sid.sat - GPS_FIRST_PRN;
-  t->CONTROL = prn << NAP_TRK_CONTROL_SAT_Pos;
+  /* PRN code */
+  control = (prn << NAP_TRK_CONTROL_SAT_Pos) & NAP_TRK_CONTROL_SAT_Msk;
+  /* RF frontend channel */
+  control |= (sid_to_rf_frontend_channel(sid) << NAP_TRK_CONTROL_RF_FE_Pos) & \
+             NAP_TRK_CONTROL_RF_FE_Msk;
+  /* Constellation and band for tracking */
+  control |= (sid_to_nap_code(sid) << NAP_TRK_CONTROL_CODE_Pos) & \
+             NAP_TRK_CONTROL_CODE_Msk;
+  /* We are not utilizing multiple signals within one RF channel at the moment.
+     Therefore, RF_FE_CH is 0 and below statement in a NOP. */
+  control |= (0 << NAP_TRK_CONTROL_RF_FE_CH_Pos) & NAP_TRK_CONTROL_RF_FE_CH_Msk;
 
+  t->CONTROL = control;
   /* We always start at zero code phase */
   t->CODE_INIT_INT = 0;
   t->CODE_INIT_FRAC = 0;
-  t->CODE_INIT_G1 = 0x3ff;
+  t->CODE_INIT_G1 = sid_to_init_g1(sid);
   t->CODE_INIT_G2 = 0x3ff;
 
   t->SPACING = (SPACING_HALF_CHIP << NAP_TRK_SPACING_OUTER_Pos) |
                (SPACING_HALF_CHIP << NAP_TRK_SPACING_INNER_Pos);
 
-  double cp_rate = (1.0 + carrier_freq / GPS_L1_HZ) * GPS_CA_CHIPPING_RATE;
-  nap_track_update(channel, carrier_freq, cp_rate, 0, 0);
+  double cp_rate = (1.0 + carrier_freq / code_to_carr_freq(sid.code)) *
+                   code_to_chip_rate(sid.code);
+  nap_track_update(channel, carrier_freq, cp_rate, chips_to_correlate, 0);
 
   /* First integration is one sample short */
   t->LENGTH += 1;
@@ -105,11 +235,12 @@ void nap_track_init(u8 channel, gnss_signal_t sid, u32 ref_timing_count,
     chSysLock();
     tc_req = NAP->TIMING_COUNT + TIMING_COMPARE_DELTA_MIN;
     double cp = propagate_code_phase(code_phase, carrier_freq,
-                                     tc_req - ref_timing_count);
+                                     tc_req - ref_timing_count, sid.code);
     /* Contrive for the timing strobe to occur at or close to a
      * PRN edge (code phase = 0) */
-    tc_req += (NAP_FRONTEND_SAMPLE_RATE_Hz / GPS_CA_CHIPPING_RATE) * (1023.0 - cp) *
-              (1.0 + carrier_freq / GPS_L1_HZ);
+    tc_req += (NAP_FRONTEND_SAMPLE_RATE_Hz / code_to_chip_rate(sid.code)) *
+              (code_to_chip_num(sid.code) - cp) *
+              (1.0 + carrier_freq / code_to_carr_freq(sid.code));
 
     NAP->TRK_TIMING_COMPARE = tc_req;
     chSysUnlock();
@@ -130,7 +261,7 @@ void nap_track_init(u8 channel, gnss_signal_t sid, u32 ref_timing_count,
 }
 
 void nap_track_update(u8 channel, double carrier_freq,
-                      double code_phase_rate, u8 rollover_count,
+                      double code_phase_rate, u32 chips_to_correlate,
                       u8 corr_spacing)
 {
   (void)corr_spacing; /* This is always written as 0 now... */
@@ -141,7 +272,7 @@ void nap_track_update(u8 channel, double carrier_freq,
   u32 cp_rate_units = code_phase_rate * NAP_TRACK_CODE_PHASE_RATE_UNITS_PER_HZ;
   t->CARR_PINC = -carrier_freq * NAP_TRACK_CARRIER_FREQ_UNITS_PER_HZ;
   t->CODE_PINC = cp_rate_units;
-  t->LENGTH = calc_length_samples(rollover_count + 1,
+  t->LENGTH = calc_length_samples(chips_to_correlate,
                                   s->code_phase, cp_rate_units);
 }
 

src/simulator.c
@@ -352,7 +352,8 @@ void populate_nav_meas(navigation_measurement_t *nav_meas, double dist, double e
   nav_meas->raw_pseudorange += rand_gaussian(sim_settings.pseudorange_sigma *
                                              sim_settings.pseudorange_sigma);
 
-  nav_meas->raw_carrier_phase =    dist / (GPS_C / GPS_L1_HZ);
+  nav_meas->raw_carrier_phase =     dist / (GPS_C /
+            code_to_carr_freq(simulation_almanacs[almanac_i].sid.code));
   nav_meas->raw_carrier_phase +=   simulation_fake_carrier_bias[almanac_i];
   nav_meas->raw_carrier_phase +=   rand_gaussian(sim_settings.phase_sigma *
                                              sim_settings.phase_sigma);