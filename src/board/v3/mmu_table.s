cbd1490109d9faaabba5e55d5c687fa4977a551a
Jacob McNamee <jacob@swiftnav.com>
2016-08-03 18:17:26 -0700

    Make caches work with Linux

--- src/board/v3/mmu_table.s
+++ src/board/v3/mmu_table.s
@@ -21,8 +21,9 @@ MMUTable:
      */
 .set SECT, 0
 
-                            /* 0x00000000 - 0x00100000 (cacheable) */
-.word   SECT + 0x15de6      /* S=b1 TEX=b101 AP=b11, Domain=b1111, C=b0, B=b1 */
+                            /* 0x00000000 - 0x00100000 (Low OCM / DDR)
+                             * Generates a translation fault if accessed */
+.word   SECT + 0x0          /* S=b0 TEX=b000 AP=b00, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 
 .rept   0x1DF               /* 0x00100000 - 0x1e000000 (Linux DDR)
@@ -31,13 +32,15 @@ MMUTable:
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x10                /* 0x1e000000 - 0x1f000000 (Firmware DDR) */
+.rept   0x10                /* 0x1e000000 - 0x1f000000 (Shared DDR)
+                             * Normal, shared, non-cacheable */
 .word   SECT + 0x14de2      /* S=b1 TEX=b100 AP=b11, Domain=b1111, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x10                /* 0x1f000000 - 0x1fffffff (Shared DDR) */
-.word   SECT + 0x14de2      /* S=b1 TEX=b100 AP=b11, Domain=b1111, C=b0, B=b0 */
+.rept   0x10                /* 0x1f000000 - 0x1fffffff (Firmware DDR)
+                             * Normal, shared, inner and outer cacheable */
+.word   SECT + 0x15de6      /* S=b1 TEX=b101 AP=b11, Domain=b1111, C=b0, B=b1 */
 .set    SECT, SECT+0x100000
 .endr
 
@@ -47,13 +50,15 @@ MMUTable:
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x0400              /* 0x40000000 - 0x7fffffff (FPGA slave0) */
-.word   SECT + 0xc02        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b0, B=b1 */
+.rept   0x0400              /* 0x40000000 - 0x7fffffff (FPGA slave0)
+                             * Strongly ordered */
+.word   SECT + 0xc02        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x0400              /* 0x80000000 - 0xbfffffff (FPGA slave1) */
-.word   SECT + 0xc02        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b0, B=b1 */
+.rept   0x0400              /* 0x80000000 - 0xbfffffff (FPGA slave1)
+                             * Generates a translation fault if accessed */
+.word   SECT + 0x0          /* S=b0 TEX=b000 AP=b00, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 .endr
 
@@ -64,7 +69,8 @@ MMUTable:
 .endr
 
 .rept   0x003               /* 0xe0000000 - 0xe02fffff (Memory mapped devices)
-                             * UART/USB/IIC/SPI/CAN/GEM/GPIO/QSPI/SD/NAND */
+                             * UART/USB/IIC/SPI/CAN/GEM/GPIO/QSPI/SD/NAND
+                             * Device */
 .word   SECT + 0xc06        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b0, B=b1 */
 .set    SECT, SECT+0x100000
 .endr
@@ -75,18 +81,21 @@ MMUTable:
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x0010              /* 0xe1000000 - 0xe1ffffff (NAND) */
-.word   SECT + 0xc06        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b0, B=b1 */
+.rept   0x0010              /* 0xe1000000 - 0xe1ffffff (NAND)
+                             * Generates a translation fault if accessed */
+.word   SECT + 0x0          /* S=b0 TEX=b000 AP=b00, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x0020              /* 0xe2000000 - 0xe3ffffff (NOR) */
-.word   SECT + 0xc06        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b0, B=b1 */
+.rept   0x0020              /* 0xe2000000 - 0xe3ffffff (NOR)
+                             * Generates a translation fault if accessed */
+.word   SECT + 0x0          /* S=b0 TEX=b000 AP=b00, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x0020              /* 0xe4000000 - 0xe5ffffff (SRAM) */
-.word   SECT + 0xc0e        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b1, B=b1 */
+.rept   0x0020              /* 0xe4000000 - 0xe5ffffff (SRAM)
+                             * Generates a translation fault if accessed */
+.word   SECT + 0x0          /* S=b0 TEX=b000 AP=b00, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 .endr
 
@@ -100,7 +109,8 @@ MMUTable:
    0xf8f03000 to 0xf8ffffff are reserved  but due to granual size of
    1MB, it is not possible to define separate regions for them */
 
-.rept   0x0010              /* 0xf8000000 - 0xf8ffffff (AMBA APB Peripherals) */
+.rept   0x0010              /* 0xf8000000 - 0xf8ffffff (AMBA APB Peripherals)
+                             * Device */
 .word   SECT + 0xc06        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b0, B=b1 */
 .set    SECT, SECT+0x100000
 .endr
@@ -111,8 +121,9 @@ MMUTable:
 .set    SECT, SECT+0x100000
 .endr
 
-.rept   0x0020              /* 0xfc000000 - 0xfdffffff (Linear QSPI - XIP) */
-.word   SECT + 0xc0a        /* S=b0 TEX=b000 AP=b11, Domain=b0, C=b1, B=b1 */
+.rept   0x0020              /* 0xfc000000 - 0xfdffffff (Linear QSPI - XIP)
+                             * Generates a translation fault if accessed */
+.word   SECT + 0x0          /* S=b0 TEX=b000 AP=b00, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 .endr
 
@@ -123,12 +134,12 @@ MMUTable:
 .endr
 
 /* 0xfff00000 to 0xfffb0000 is reserved but due to granual size of
-   1MB, it is not possible to define separate region for  it
+   1MB, it is not possible to define separate region for it */
 
                             /* 0xfff00000 - 0xffffffff
-                               256K OCM when mapped to high address space
-                               inner-cacheable */
-.word   SECT + 0x4c0e       /* S=b0 TEX=b100 AP=b11, Domain=b0, C=b1, B=b1 */
+                             * 256K OCM when mapped to high address space
+                             * Generates a translation fault if accessed */
+.word   SECT + 0x0          /* S=b0 TEX=b000 AP=b00, Domain=b0, C=b0, B=b0 */
 .set    SECT, SECT+0x100000
 
 .end
