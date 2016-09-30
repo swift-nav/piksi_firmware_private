e81c10c09a450f7bb57380ddd8675719f5da7c16
Colin Beighley <colinbeighley@gmail.com>
2016-09-30 12:02:41 -0700

    Build links yet more intelligently

--- comment.sh
+++ comment.sh
@@ -28,31 +28,42 @@ BUILD_PATH="$REPO/$BUILD_VERSION"
 RELEASES="piksi_firmware_v0.21.hex"
 
 LINKS=\
-"http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/passfail/f/firmware/sv/$RELEASES%2C$BUILD_VERSION
+(http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/passfail/f/firmware/sv/$RELEASES%2C$BUILD_VERSION
 http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/piksi-multi-PRD/f/firmware/sv/$RELEASES%2C$BUILD_VERSION
 https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=swiftnav-artifacts&prefix=$BUILD_PATH/
 https://swiftnav-artifacts.herokuapp.com/$BUILD_PATH/
-https://swiftnav-artifacts.herokuapp.com/$BUILD_PATH/requirements.yaml"
+https://swiftnav-artifacts.herokuapp.com/$BUILD_PATH/requirements.yaml)
 
 TITLES=\
-"sbp-log-analysis/passfail/$BUILD_PATH
+(sbp-log-analysis/passfail/$BUILD_PATH
 sbp-log-analysis/piksi-multi-PRD/$BUILD_PATH
 s3://$PRS_BUCKET/$BUILD_PATH
 pull-requests/$BUILD_PATH
-pull-requests/$BUILD_PATH/requirements.yaml"
+pull-requests/$BUILD_PATH/requirements.yaml)
+
+slack_links(){
+    echo -n $BUILD_PATH
+    for index in ${!LINKS[@]}; do
+        echo -ne "\n"${LINKS[$index]}
+    done
+}
+
+github_links(){
+    echo -n "## $BUILD_VERSION"
+    for index in ${!LINKS[@]}; do
+        echo -n "\n+ "[${TITLES[$index]}]"("${LINKS[$index]}")"
+    done
+}
 
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     if [ "$TRAVIS_BRANCH" == "master" ]; then
-        COMMENT="$(cat <<EOF
-$BUILD_PATH
-$LINKS
-EOF)"
+        COMMENT="$(slack_links)"
         URL="https://slack.com/api/chat.postMessage?token=$SLACK_TOKEN&channel=$SLACK_CHANNEL"
         DATA="text=$COMMENT"
         curl --data-urlencode "$DATA" "$URL"
     fi
 elif [ ! -z "$GITHUB_TOKEN" ]; then
-    COMMENT="## $BUILD_VERSION\n+ [${TITLES[0]}](${LINKS[0]})\n+ [${TITLES[1]}](${LINKS[1]})\n+ [${TITLES[2]}](${LINKS[2]})\n+ [${TITLES[3]}](${LINKS[3]})\n+ [${TITLES[4]}](${LINKS[4]})"
+    COMMENT="$(github_links)"
     URL="https://api.github.com/repos/swift-nav/$REPO/issues/$TRAVIS_PULL_REQUEST/comments"
     curl -u "$GITHUB_TOKEN:" -X POST "$URL" -d "{\"body\":\"$COMMENT\"}"
 fi
