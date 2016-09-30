80aa59b508dc54ef75413671275b2c44858165d6
Colin Beighley <colinbeighley@gmail.com>
2016-09-29 19:16:32 -0700

    Update Travis comment links to sbp-log-analysis

--- comment.sh
+++ comment.sh
@@ -25,20 +25,36 @@ PRS_BUCKET="${PRS_BUCKET:-swiftnav-artifacts-pull-requests}"
 BUILD_VERSION="$(git describe --tags --dirty --always)"
 BUILD_PATH="$REPO/$BUILD_VERSION"
 
-if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-    if [ "$TRAVIS_BRANCH" == "master" ]; then
-        COMMENT="$BUILD_PATH
-http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/passfail/f/firmware/sv/$BUILD_VERSION
-http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/non_passfail/f/firmware/sv/$BUILD_VERSION
+RELEASES="piksi_firmware_v0.21.hex"
+
+LINKS=\
+"http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/passfail/f/firmware/sv/$RELEASES%2C$BUILD_VERSION
+http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/piksi-multi-PRD/f/firmware/sv/$RELEASES%2C$BUILD_VERSION
 https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=swiftnav-artifacts&prefix=$BUILD_PATH/
 https://swiftnav-artifacts.herokuapp.com/$BUILD_PATH/
 https://swiftnav-artifacts.herokuapp.com/$BUILD_PATH/requirements.yaml"
+
+TITLES=\
+"sbp-log-analysis/passfail/$BUILD_PATH
+sbp-log-analysis/piksi-multi-PRD/$BUILD_PATH
+s3://$PRS_BUCKET/$BUILD_PATH
+pull-requests/$BUILD_PATH
+pull-requests/$BUILD_PATH/requirements.yaml"
+
+if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    if [ "$TRAVIS_BRANCH" == "master" ]; then
+        COMMENT="$BUILD_PATH
+${LINKS[0]}
+${LINKS[1]}
+${LINKS[2]}
+${LINKS[3]}
+${LINKS[4]}"
         URL="https://slack.com/api/chat.postMessage?token=$SLACK_TOKEN&channel=$SLACK_CHANNEL"
         DATA="text=$COMMENT"
         curl --data-urlencode "$DATA" "$URL"
     fi
 elif [ ! -z "$GITHUB_TOKEN" ]; then
-    COMMENT="## $BUILD_VERSION\n+ [sbp-log-analysis/passfail/$BUILD_PATH](http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/passfail/f/firmware/sv/$BUILD_VERSION)\n+ [sbp-log-analysis/non_passfail/$BUILD_PATH](http://sbp-log-analysis.swiftnav.com/#/d/0/q/x/scenario/y/metric/f/metric/p/non_passfail/f/firmware/sv/$BUILD_VERSION)\n+ [s3://$PRS_BUCKET/$BUILD_PATH](https://console.aws.amazon.com/s3/home?region=us-west-2&bucket=swiftnav-artifacts-pull-requests&prefix=$BUILD_PATH/)\n+ [pull-requests/$BUILD_PATH](https://swiftnav-artifacts.herokuapp.com/pull-requests/$BUILD_PATH/)\n+ [pull-requests/$BUILD_PATH/requirements.yaml](https://swiftnav-artifacts.herokuapp.com/pull-requests/$BUILD_PATH/requirements.yaml)"
+    COMMENT="## $BUILD_VERSION\n+ [${TITLES[0]}](${LINKS[0]})\n+ [${TITLES[1]}](${LINKS[1]})\n+ [${TITLES[2]}](${LINKS[2]})\n+ [${TITLES[3]}](${LINKS[3]})\n+ [${TITLES[4]}](${LINKS[4]})"
     URL="https://api.github.com/repos/swift-nav/$REPO/issues/$TRAVIS_PULL_REQUEST/comments"
     curl -u "$GITHUB_TOKEN:" -X POST "$URL" -d "{\"body\":\"$COMMENT\"}"
 fi
