2d902f599e8963bb3e52fdcbaa9ac0ed7674b7eb
Perttu Salmela <psalmela@exafore.com>
2016-10-18 22:13:29 +0300

    Review fixes part 3

--- src/reacq/search_manager_unittest.c
+++ src/reacq/search_manager_unittest.c
@@ -15,7 +15,8 @@
 #include "search_manager_api.h"
 
 /* Compile unit test in src/reacq directory with:
- gcc  -Wall -std=c99 -DSM_UNIT_TEST search_manager_unittest.c search_manager.c\
+ gcc  -Wall -std=c99 search_manager_unittest.c search_manager.c\
+ ../../libswiftnav/src/signal.c \
  -I.. -I../../libswiftnav/include -I ../../src/board/v3/ \
  -I../../ChibiOS/os/rt/include/ -I../../ChibiOS/os/rt/templates -o sm_unittest
 */
@@ -34,40 +35,44 @@ static struct test_case_t *test_case;
 
 /** Unit test cases */
 static struct test_case_t test_cases[] = {
-  {0,0x0, 0x0, 0x0, 0x0, 0,   0x0, 0x0}, /* Nothing */
+  /* No SV tracked, health does not effect */
+  {0,0x0, 0x0, 0x0, 0x0, 0,   0xffffffff, 0x0}, 
   {10, 0xffffffff, 0x0, 0x0, 0x0, 0,   0xffffffff,0x0},/* All healthy
                                                           not tracked */
   {10, 0xffffffff, 0x0, 0x0, 0xffffffff, 0,   0x0, 0x0},/* All healthy all
                                                            tracked */
   {20, 0x0, 0x0, 0x0, 0xffffffff, 0,   0x0, 0x0},/* All unhealthy all tracked */
-  {30, 0x1, 0x1, 0x1, 0x0, 0,   0x1, 0x0},/* 1 healthy, not tracked, visible */
-  {30, 0x1, 0x0, 0x1, 0x0, 0,   0x0, 0x0},/* 1 healthy, not tracked,
-                                             invisible */
-  {30, 0x1, 0x0, 0x0, 0x0, 0,   0x1, 0x0},/* 1 healthy, not tracked, unknown */
+  {30, 0x1, 0x1, 0x1, 0x0, 0,   0xffffffff, 0x0},/* 1 healthy, not tracked, visible */
+  {30, 0x1, 0x0, 0x1, 0x0, 0,   0xfffffffe, 0x0},/* 1 healthy, not tracked,
+						    invisible */
+  {30, 0x1, 0x0, 0x0, 0x0, 0,   0xffffffff, 0x0},/* 1 healthy, not tracked, unknown */
   /* Timeouts */
   /* 1 healthy, not tracked, visible */
-  {ACQ_LGF_TIMEOUT_VIS_AND_UNKNOWN_MS+1, 0x1, 0x1, 0x1, 0x0, 0,   0x1, 0x1},
+  {ACQ_LGF_TIMEOUT_VIS_AND_UNKNOWN_MS+1, 0x1, 0x1, 0x1, 0x0, 0,
+   0xffffffff,0xffffffff},
   /* 1 healthy,  not tracked,invisible */
-  {ACQ_LGF_TIMEOUT_INVIS_MS+1, 0x1, 0x0, 0x1, 0x0, 0,   0x0, 0x1},
+  {ACQ_LGF_TIMEOUT_INVIS_MS+1, 0x1, 0x0, 0x1, 0x0, 0,
+   0xfffffffe,0xffffffff},
   /* 1 healthy, not tracked, unknown */
-  {ACQ_LGF_TIMEOUT_VIS_AND_UNKNOWN_MS+1, 0x1, 0x0, 0x0, 0x0, 0,   0x1, 0x1}
+  {ACQ_LGF_TIMEOUT_VIS_AND_UNKNOWN_MS+1, 0x1, 0x0, 0x0, 0x0, 0,
+   0xffffffff,0xffffffff}
 };
 
 /** Get SV visibility flags
  *
- * \param sid SV identifier
- * \param [out] visible set if SV is visible
- * \param [known] known set if SV is known visible or known invisible
+ * \param[in] sid GNSS signal identifier
+ * \param[out] visible set if SV is visible
+ * \param[out] known set if SV is known visible or known invisible
  */
 void sm_get_visibility_flags(gnss_signal_t sid,
 			     bool *visible, bool *known)
 {
-  if (0 != (test_case->vis_mask & (1 << (sid.sat - GPS_FIRST_PRN)))) {
+  if (0 != (test_case->vis_mask & (1 << sid_to_code_index(sid)))) {
     *visible = true;
   } else {
     *visible = false;
   }
-  if (0 != (test_case->known_mask & (1 << (sid.sat - GPS_FIRST_PRN)))) {
+  if (0 != (test_case->known_mask & (1 << sid_to_code_index(sid)))) {
     *known = true;
   } else {
     *known = false;
@@ -75,13 +80,13 @@ void sm_get_visibility_flags(gnss_signal_t sid,
 }
 /** Check if SV is healthy
  *
- * \param sid SV identifier
+ * \param sid GNSS signal identifier
  *
- * \return TRUE is SV is healthy, FALSE otherwise
+ * \return true is SV is healthy, false otherwise
  */
 bool sm_is_healthy(gnss_signal_t sid)
 {
-  if (0 != (test_case->health_mask & (1 << (sid.sat - GPS_FIRST_PRN)))) {
+  if (0 != (test_case->health_mask & (1 << sid_to_code_index(sid)))) {
     return true;
   }
   return false;
@@ -91,11 +96,11 @@ bool sm_is_healthy(gnss_signal_t sid)
  *
  * \param sid SV identifier
  *
- * \return TRUE is SV is tracked, FALSE otherwise
+ * \return true is SV is tracked, false otherwise
  */
 bool sid_is_tracked(gnss_signal_t sid)
 {
-  if (0 != (test_case->track_mask & (1 << (sid.sat - GPS_FIRST_PRN)))) {
+  if (0 != (test_case->track_mask & (1 << sid_to_code_index(sid)))) {
     return true;
   }
   return false;
@@ -111,11 +116,13 @@ u64 timing_getms(void)
 
 /** Get HW time of the last good fix (LGF)
  *
- * \return HW time (ms) of the last good fix
+ * \param [out] lgf_stamp time of LGF (ms)
+ * \return true lgf_stamp is valid, false otherwise
  */
-u64 sm_lgf_stamp(void)
+bool sm_lgf_stamp(u64 *lgf_stamp)
 {
-  return (u64)test_case->lgf_stamp_ms;
+  *lgf_stamp = (u64)test_case->lgf_stamp_ms; 
+  return true;
 }
 
 /** Test program checking search manager operation
@@ -169,7 +176,7 @@ int main(int argc, char **argv)
     } else {
       printf("\tOK\n");
     }
-  } // for
+  } /* for */
 
   return 0;
 }
