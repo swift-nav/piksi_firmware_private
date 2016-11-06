b58e5e6616d019acc1e3dfd11eedad48dfba9080
Leith Bade <leith@swiftnav.com>
2016-11-06 18:35:40 +1100

    Stub soln_output

---
+++ src/soln_output.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2016 Swift Navigation Inc.
+ * Contact: Leith Bade <leith@swift-nav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef SWIFTNAV_SOLUTION_H
+#define SWIFTNAV_SOLUTION_H
+
+#include <libswiftnav/common.h>
+
+
+
+#endif
