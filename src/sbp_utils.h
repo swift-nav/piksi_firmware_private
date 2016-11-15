508c1bb8762e217c61cca891946db5a48f7bf98d
Ben Segal <benjamin0@users.noreply.github.com>
2016-11-15 15:36:31 -0800

    Output covariance. (#285)
    Output RTK accuracy values via SBP.

--- src/sbp_utils.h
+++ src/sbp_utils.h
@@ -38,17 +38,21 @@ typedef struct {
 void sbp_make_gps_time(msg_gps_time_t *t_out, const gps_time_t *t_in, u8 flags);
 void sbp_make_pos_llh(msg_pos_llh_t *pos_llh, const gnss_solution *soln, u8 flags);
 void sbp_make_pos_llh_vect(msg_pos_llh_t *pos_llh, const double llh[3],
+                           double h_accuracy, double v_accuracy,
                            const gps_time_t *gps_t, u8 n_used, u8 flags);
 void sbp_make_pos_ecef(msg_pos_ecef_t *pos_ecef, const gnss_solution *soln, u8 flags);
 void sbp_make_pos_ecef_vect(msg_pos_ecef_t *pos_ecef, const double ecef[3],
-                            const gps_time_t *gps_t, u8 n_used, u8 flags);
+                            double accuracy, const gps_time_t *gps_t, u8 n_used,
+                            u8 flags);
 void sbp_make_vel_ned(msg_vel_ned_t *vel_ned, const gnss_solution *soln, u8 flags);
 void sbp_make_vel_ecef(msg_vel_ecef_t *vel_ecef, const gnss_solution *soln, u8 flags);
 void sbp_make_dops(msg_dops_t *dops_out, const dops_t *dops_in, const gps_time_t *t);
 void sbp_make_baseline_ecef(msg_baseline_ecef_t *baseline_ecef, const gps_time_t *t,
-                            u8 n_sats, const double b_ecef[3], u8 flags);
+                            u8 n_sats, const double b_ecef[3], double accuracy,
+                            u8 flags);
 void sbp_make_baseline_ned(msg_baseline_ned_t *baseline_ned, const gps_time_t *t,
-                           u8 n_sats, const double b_ned[3], u8 flags);
+                           u8 n_sats, const double b_ned[3], double h_accuracy,
+                           double v_accuracy, u8 flags);
 void sbp_make_heading(msg_baseline_heading_t *baseline_heading, const gps_time_t *t,
                       const double heading, u8 n_used, u8 flags);
 #define MSG_OBS_HEADER_SEQ_SHIFT 4u
