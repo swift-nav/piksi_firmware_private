163f5d312e04133adecdaa03a583d088aff28c47
Jacob McNamee <jacob@swiftnav.com>
2016-11-22 15:42:24 -0800

    Add setting for antenna selection, default to primary

--- src/board/v3/peripherals/antenna.c
+++ src/board/v3/peripherals/antenna.c
@@ -15,8 +15,56 @@
 #include <ch.h>
 #include <hal.h>
 #include <libswiftnav/logging.h>
+#include "settings.h"
 
 #include <string.h>
+#include <assert.h>
+
+typedef enum {
+  ANTENNA_MODE_PRIMARY,
+  ANTENNA_MODE_SECONDARY
+} antenna_mode_t;
+
+static const char * const antenna_mode_strings[] = {
+  "Primary",
+  "Secondary",
+  NULL
+};
+
+static antenna_mode_t antenna_mode = ANTENNA_MODE_PRIMARY;
+
+static void antenna_mode_configure(antenna_mode_t mode)
+{
+  switch (mode) {
+  case ANTENNA_MODE_PRIMARY: {
+    palClearLine(ANT_IN_SEL_0_GPIO_LINE);
+    palSetLine(ANT_IN_SEL_1_GPIO_LINE);
+  }
+  break;
+
+  case ANTENNA_MODE_SECONDARY: {
+    palClearLine(ANT_IN_SEL_1_GPIO_LINE);
+    palSetLine(ANT_IN_SEL_0_GPIO_LINE);
+  }
+  break;
+
+  default: {
+    assert(!"Invalid antenna mode");
+  }
+  break;
+  }
+}
+
+static bool antenna_mode_notify(struct setting *s, const char *val)
+{
+  if (!s->type->from_string(s->type->priv, s->addr, s->len, val))
+  {
+    return false;
+  }
+
+  antenna_mode_configure(antenna_mode);
+  return true;
+}
 
 void antenna_init(void)
 {
@@ -27,7 +75,7 @@ void antenna_init(void)
   palSetLine(ANT_PWR_SEL_2_GPIO_LINE);
 
   palSetLineMode(ANT_IN_SEL_0_GPIO_LINE, PAL_MODE_OUTPUT);
-  palSetLine(ANT_IN_SEL_0_GPIO_LINE);
+  palClearLine(ANT_IN_SEL_0_GPIO_LINE);
 
   palSetLineMode(ANT_IN_SEL_1_GPIO_LINE, PAL_MODE_OUTPUT);
   palClearLine(ANT_IN_SEL_1_GPIO_LINE);
@@ -36,9 +84,32 @@ void antenna_init(void)
   palSetLineMode(ANT_PRESENT_2_GPIO_LINE, PAL_MODE_INPUT);
   palSetLineMode(ANT_NFAULT_1_GPIO_LINE, PAL_MODE_INPUT);
   palSetLineMode(ANT_NFAULT_2_GPIO_LINE, PAL_MODE_INPUT);
+
+  static struct setting_type antenna_mode_setting;
+  int TYPE_ANTENNA_MODE = settings_type_register_enum(antenna_mode_strings,
+                                                      &antenna_mode_setting);
+  SETTING_NOTIFY("frontend", "antenna_selection",
+                 antenna_mode, TYPE_ANTENNA_MODE, antenna_mode_notify);
+
+  antenna_mode_configure(antenna_mode);
 }
 
 bool antenna_present(void)
 {
-  return (palReadLine(ANT_PRESENT_2_GPIO_LINE) == PAL_HIGH);
+  switch (antenna_mode) {
+  case ANTENNA_MODE_PRIMARY: {
+    return (palReadLine(ANT_PRESENT_1_GPIO_LINE) == PAL_HIGH);
+  }
+
+  case ANTENNA_MODE_SECONDARY: {
+    return (palReadLine(ANT_PRESENT_2_GPIO_LINE) == PAL_HIGH);
+  }
+
+  default: {
+    assert(!"Invalid antenna mode setting");
+    return false;
+  }
+  }
+
+  return false;
 }
