db77830bfe6b3cc4e8b88a5c2cbf15b859bbdc80
Mark Fine <mark.fine@gmail.com>
2016-12-13 11:59:31 -0800

    Build buildroot artifacts.

---
+++ yolo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copyright (C) 2016 Swift Navigation Inc.
+# Contact: Mark Fine <mark@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Script for publishing built binaries to S3.
+
+set -e
+
+git clone git@github.com:swift-nav/piksi_buildroot.git
+(cd piksi_buildroot && ./build.sh)
