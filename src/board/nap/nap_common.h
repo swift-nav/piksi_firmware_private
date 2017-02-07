c3fbd49d4e3d7915cef62f97d878a83f462b8b5f
adel-mamin <adel.mamin@exafore.com>
2017-02-07 16:56:21 +0200

    Compute tracker uptime based on timing_getms() (#543)
    Add tracker timing_getms() based uptime & update rate monitor

--- src/board/nap/nap_common.h
+++ src/board/nap/nap_common.h
@@ -40,6 +40,17 @@ typedef struct {
 
 extern binary_semaphore_t timing_strobe_sem;
 
+/** The maximum expected correlation length for sanity checks [ms] */
+#define NAP_CORR_LENGTH_MAX_MS 100
+/** The minimum expected correlation length for sanity checks [ms] */
+#define NAP_CORR_LENGTH_MIN_MS 0.5
+
+/** Convert milliseconds to NAP samples */
+#define NAP_MS_2_SAMPLES(ms) (((double)(ms) / 1000.) * NAP_TRACK_SAMPLE_RATE_Hz)
+
+/** Convert NAP samples to milliseconds */
+#define NAP_SAMPLES_2_MS(samples) (((samples) * 1000.) / NAP_TRACK_SAMPLE_RATE_Hz)
+
 /** \} */
 
 void nap_setup(void);
