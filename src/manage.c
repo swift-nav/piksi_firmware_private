e8bfd1abe8fc8e1b8a54e8c28735ac7cece5c7b2
Colin Beighley <colinbeighley@gmail.com>
2017-03-22 16:36:16 -0700

    Add print for dopp hints debugging

--- src/manage.c
+++ src/manage.c
@@ -389,6 +389,9 @@ static u16 manage_warm_start(gnss_signal_t sid, const gps_time_t* t,
   /* Return the doppler hints and a score proportional to elevation */
   *dopp_hint_low = MAX(dopp_hint - dopp_uncertainty, doppler_min);
   *dopp_hint_high = MIN(dopp_hint + dopp_uncertainty, doppler_max);
+  log_info("dopp_hints information: %lf, %lf, %lf, %lf, %lf, %lf",
+           *dopp_hint_low, *dopp_hint_high, dopp_hint, dopp_uncertainty,
+           doppler_min, doppler_max);
   return SCORE_COLDSTART + SCORE_WARMSTART * el / 90.f;
 }
 
