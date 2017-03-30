41c3e3508b53687b6e5a87d557f83e83a9e5b101
Ben Segal <ben@swift-nav.com>
2017-03-30 13:07:47 -0700

    check == 1

--- src/solution.c
+++ src/solution.c
@@ -1392,7 +1392,7 @@ static void time_matched_obs_thread(void *arg)
       double dt = gpsdifftime(&obss->tor, &base_obss.tor);
 
       if (fabs(dt) < TIME_MATCH_THRESHOLD &&
-          (base_obss.has_pos || base_obss.has_known_pos_ecef)) {
+          (base_obss.has_pos == 1 || base_obss.has_known_pos_ecef)) {
         obss_t base_obss_copy = base_obss;
         chMtxUnlock(&base_obs_lock);
         // We need to form the SBP messages derived from the SPP at this solution time before we
