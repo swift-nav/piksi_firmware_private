72a795b5f5826b5ef69bc55d245a4897f855be13
Adel Mamin <adel.mamin@exafore.com>
2017-05-22 14:49:00 +0300

    Add GLO map setup + call glo_map_set_slot_id() from GLO data decoder

---
+++ src/glo_map_setup.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Adel Mamin <adel.mamin@exafore.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef GLO_MAP_SETUP_H
+#define GLO_MAP_SETUP_H
+
+void glo_map_setup(void);
+
+#endif /* #ifndef GLO_MAP_SETUP_H */
