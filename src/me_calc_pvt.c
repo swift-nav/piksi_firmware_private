bc92a9a857ac99bd051e27956dfa4f8acb75829b
Michele Bavaro <michele@swiftnav.com>
2017-06-19 14:59:31 -0700

    changed interface of calc_sat_state

--- src/me_calc_pvt.c
+++ src/me_calc_pvt.c
@@ -628,8 +628,14 @@ static void me_calc_pvt_thread(void *arg)
         }
 
         /* Recompute satellite position, velocity and clock errors */
-        if (0 != calc_sat_state(e, &nm->tot, nm->sat_pos, nm->sat_vel,
-                                &nm->sat_clock_err, &nm->sat_clock_err_rate)) {
+        if (0 != calc_sat_state(e,
+                                &nm->tot,
+                                nm->sat_pos,
+                                nm->sat_vel,
+                                &nm->sat_clock_err,
+                                &nm->sat_clock_err_rate,
+                                &nm->IODC,
+                                &nm->IODE)) {
           continue;
         }
 
