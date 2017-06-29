920b5c86fa4b81a26da263f5361bc752b88e80b3
mbavaro <michele@swiftnav.com>
2017-06-28 17:34:24 -0700

    added logging upon failure (#884)

--- src/sbp_fileio.c
+++ src/sbp_fileio.c
@@ -31,7 +31,7 @@ static u8 next_seq(void)
   static MUTEX_DECL(seq_mtx);
   static u8 seq;
   u8 ret;
-  
+
   if (seq == 0) {
     seq = rand();
   }
@@ -71,11 +71,10 @@ ssize_t sbp_fileio_write(const char *filename, off_t offset, const u8 *buf, size
   size_t s = 0;
   u8 payload_offset = sizeof(msg_fileio_write_req_t) + strlen(filename) + 1;
   ssize_t chunksize = 255 - payload_offset;
-  if (chunksize < 0)
-    return -1;
   struct sbp_fileio_closure closure;
   msg_fileio_write_req_t *msg = alloca(256);
   chBSemObjectInit(&closure.sem, true);
+  u8 *msg_pt;
 
   sbp_msg_callbacks_node_t node;
   sbp_register_cbk_with_closure(SBP_MSG_FILEIO_WRITE_RESP,
@@ -85,15 +84,16 @@ ssize_t sbp_fileio_write(const char *filename, off_t offset, const u8 *buf, size
     msg->sequence = closure.seq = next_seq();
     msg->offset = offset + s;
     strcpy(msg->filename, filename);
+    msg_pt = (u8*) msg;
     chunksize = MIN(chunksize, (ssize_t)(size - s));
-    memcpy((u8*)msg + payload_offset,
-           (u8*)buf + s, chunksize);
+    memcpy(msg_pt + payload_offset,  buf + s,  chunksize);
 
     u8 tries = 0;
     bool success = false;
     do {
-      sbp_send_msg(SBP_MSG_FILEIO_WRITE_REQ, payload_offset + chunksize,
-                   (u8*)msg);
+      sbp_send_msg(SBP_MSG_FILEIO_WRITE_REQ,
+                   payload_offset + chunksize,
+                   msg_pt);
       if (chBSemWaitTimeout(&closure.sem, SBP_FILEIO_TIMEOUT) == MSG_OK) {
         success = true;
         break;
@@ -101,6 +101,7 @@ ssize_t sbp_fileio_write(const char *filename, off_t offset, const u8 *buf, size
     } while (++tries < SBP_FILEIO_TRIES);
 
     if (!success) {
+      log_error("sbp_fileio_write(): tries %d  chunksize %d   s %d", tries, chunksize, s);
       s = -1;
       break;
     }
