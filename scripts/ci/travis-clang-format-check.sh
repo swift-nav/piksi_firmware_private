82cb7923095d6a2fec8bf0ec2302b66374cd65b2
Keerthan Jaic <keerthan@swiftnav.com>
2017-07-28 12:52:51 -0700

    use clang-format 5

--- scripts/ci/travis-clang-format-check.sh
+++ scripts/ci/travis-clang-format-check.sh
@@ -7,7 +7,7 @@ else
   base_commit="$TRAVIS_BRANCH"
   echo "Running clang-format against branch $base_commit, with hash $(git rev-parse $base_commit)"
 fi
-output="$(git-clang-format-4.0 --commit $base_commit --diff)"
+output="$(git-clang-format-5.0 --commit $base_commit --diff)"
 if [ "$output" == "no modified files to format" ] || [ "$output" == "clang-format did not modify any files" ] ; then
   echo "clang-format passed."
   exit 0
