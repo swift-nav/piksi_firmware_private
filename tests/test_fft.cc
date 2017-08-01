71968857efe5faa5f00c0fe4a00ba4ce6eb5e9f5
Keerthan Jaic <keerthan@swiftnav.com>
2017-07-31 22:44:22 -0700

    clang-format

--- tests/test_fft.cc
+++ tests/test_fft.cc
@@ -2,7 +2,7 @@
 #include "lib/fixed_fft_r2.h"
 
 TEST(fft_tests, init) {
-    intFFTr2_t fft_conf;
-    InitIntFFTr2(&fft_conf, 10);
-    EXPECT_EQ(fft_conf.N, 10);
+  intFFTr2_t fft_conf;
+  InitIntFFTr2(&fft_conf, 10);
+  EXPECT_EQ(fft_conf.N, 10);
 }
