8c873eb31782dda726e392795ff52ca1b39a80e5
Michele Bavaro <michele@swiftnav.com>
2017-08-08 10:53:22 +0300

    removed need for 64 NAP counter in acq()

--- src/soft_macq/soft_macq_main.c
+++ src/soft_macq/soft_macq_main.c
@@ -28,7 +28,9 @@
 #include "soft_macq_serial.h"
 #include "soft_macq_utils.h"
 
-#define SOFTMACQ_MAX_AGE_MS (500.0)
+#define SOFTMACQ_MAX_AGE_S (0.5)
+#define SOFTMACQ_MAX_AGE_SAMP (SOFTMACQ_MAX_AGE_S * NAP_TRACK_SAMPLE_RATE_Hz)
+
 #define SOFTMACQ_SAMPLE_GRABBER_LENGTH (512 * 1024)
 #define SOFTMACQ_BASEBAND_SIZE (16 * 1024)
 
@@ -44,10 +46,10 @@ static uint8_t *puSampleBuf;
 static sc16_t pBaseBand[SOFTMACQ_BASEBAND_SIZE] __attribute__((aligned(32)));
 
 /** the last grabber acquisition time tag */
-static uint64_t uLastTimeTag;
+static u32 last_timetag;
 
 /** the last acquired signal */
-static me_gnss_signal_t sLastMesId;
+static me_gnss_signal_t mesid_last;
 
 /** */
 static uint32_t iSamplesMs;
@@ -58,9 +60,9 @@ static bool bModuleInit;
  * STATIC FUNCTION DECLARATIONS
  *********************************/
 
-static bool BbMixAndDecimate(const me_gnss_signal_t _sMeSid);
+static bool BbMixAndDecimate(const me_gnss_signal_t mesid);
 
-static bool SoftMacqSerial(const me_gnss_signal_t _sMeSid,
+static bool SoftMacqSerial(const me_gnss_signal_t mesid,
                            float _fCarrFreqMin,
                            float _fCarrFreqMax,
                            acq_result_t *_sAcqResult);
@@ -76,19 +78,19 @@ float soft_multi_acq_bin_width(void) {
 /** old interface has cf_bin_width */
 /** new interface shall be
  *
- * bool soft_multi_acq_search(const me_gnss_signal_t _sMeSid, float
+ * bool soft_multi_acq_search(const me_gnss_signal_t mesid, float
  * _fCarrFreqMin, float _fCarrFreqMax, enum sensitivity _eSense, acq_result_t
  * *_sAcqResult)
  *
  *  */
-bool soft_multi_acq_search(const me_gnss_signal_t _sMeSid,
+bool soft_multi_acq_search(const me_gnss_signal_t mesid,
                            float _fCarrFreqMin,
                            float _fCarrFreqMax,
-                           acq_result_t *_psAcqResult) {
-  uint32_t uTag = 0, uBuffLength = 0;
-  uint64_t uCurrTimeTag;
+                           acq_result_t *p_acqres) {
+  u32 tmp_timetag = 0;
+  u32 buff_size = 0;
   /** sanity checking input parameters */
-  assert(NULL != _psAcqResult);
+  assert(NULL != p_acqres);
 
   if (!bModuleInit) {
     InitBBConvLut();
@@ -98,21 +100,21 @@ bool soft_multi_acq_search(const me_gnss_signal_t _sMeSid,
 
   /** Check if the last grabbed signal snapshot isn't too old.
    * If yes, simply grab another one */
-  uCurrTimeTag = nap_timing_count();
-  if ((uLastTimeTag == 0) ||
-      (nap_count_to_ms(uCurrTimeTag - uLastTimeTag) > SOFTMACQ_MAX_AGE_MS)) {
+  u32 curr_timetag = NAP->TIMING_COUNT;
+  if ((last_timetag == 0) ||
+      ((curr_timetag - last_timetag) > SOFTMACQ_MAX_AGE_SAMP)) {
     /** GRAB!!! */
-    puSampleBuf = grab_samples(&uBuffLength, &uTag);
+    puSampleBuf = grab_samples(&buff_size, &tmp_timetag);
     if (NULL == puSampleBuf) {
       log_warn(
-          "data grabber failed, uBuffLength %u uTag %u", uBuffLength, uTag);
+          "data grabber failed, buff_size %u tmp_timetag %u", buff_size, tmp_timetag);
       return false;
     }
     /** update signal time tag */
-    uLastTimeTag = uTag;
+    last_timetag = tmp_timetag;
   }
   /** regardless of the result, store here the time tag */
-  _psAcqResult->sample_count = (uint32_t)uLastTimeTag;
+  p_acqres->sample_count = last_timetag;
 
   /** Perform signal conditioning (down-conversion, filtering and decimation):
    * - if we updated the signal snapshot or
@@ -120,14 +122,14 @@ bool soft_multi_acq_search(const me_gnss_signal_t _sMeSid,
    *   with the current one
    * - for Glonass, `sat` holds the FCN and we might want to do this again
    *  */
-  if ((uTag) || (!code_equiv(sLastMesId.code, _sMeSid.code)) ||
-      ((sLastMesId.code == CODE_GLO_L1CA) && (sLastMesId.sat != _sMeSid.sat))) {
+  if ((tmp_timetag) || (!code_equiv(mesid_last.code, mesid.code)) ||
+      ((mesid_last.code == CODE_GLO_L1CA) && (mesid_last.sat != mesid.sat))) {
     /** perform again baseband down-conversion and decimation depending on
-     * _sMeSid */
-    BbMixAndDecimate(_sMeSid);
+     * mesid */
+    BbMixAndDecimate(mesid);
   }
   /** store now last used mesid */
-  sLastMesId = _sMeSid;
+  mesid_last = mesid;
 
   /** call DBZP-like acquisition with current sensitivity parameters
    *
@@ -136,7 +138,7 @@ bool soft_multi_acq_search(const me_gnss_signal_t _sMeSid,
 
   /** call serial-frequency search acquisition with current sensitivity
    * parameters */
-  return SoftMacqSerial(_sMeSid, _fCarrFreqMin, _fCarrFreqMax, _psAcqResult);
+  return SoftMacqSerial(mesid, _fCarrFreqMin, _fCarrFreqMax, p_acqres);
 }
 
 /**********************************
@@ -145,14 +147,14 @@ bool soft_multi_acq_search(const me_gnss_signal_t _sMeSid,
 
 /************* Serial frequency search *****************/
 
-static bool SoftMacqSerial(const me_gnss_signal_t _sMeSid,
+static bool SoftMacqSerial(const me_gnss_signal_t mesid,
                            float _fCarrFreqMin,
                            float _fCarrFreqMax,
                            acq_result_t *_psLegacyResult) {
   float cf_bin_width = soft_acq_bin_width();
 
   return soft_acq_search(pBaseBand,
-                         _sMeSid,
+                         mesid,
                          _fCarrFreqMin,
                          _fCarrFreqMax,
                          cf_bin_width,
@@ -162,7 +164,7 @@ static bool SoftMacqSerial(const me_gnss_signal_t _sMeSid,
 /*! \fn bool BbMixAndDecimate
  *  \brief
  **/
-static bool BbMixAndDecimate(const me_gnss_signal_t _sMeSid) {
+static bool BbMixAndDecimate(const me_gnss_signal_t mesid) {
   uint32_t k, h, uDecFactor;
   uint32_t uNco, uNcoVal, uNcoStep = 0;
   uint8_t uSample;
@@ -170,7 +172,7 @@ static bool BbMixAndDecimate(const me_gnss_signal_t _sMeSid) {
   /** first of all reset the destination buffer */
   memset(pBaseBand, 0, SOFTMACQ_BASEBAND_SIZE * sizeof(sc16_t));
 
-  switch (_sMeSid.code) {
+  switch (mesid.code) {
     case CODE_GPS_L1CA:
     case CODE_SBAS_L1CA:
       uDecFactor = SOFTMACQ_DECFACT_GPSL1CA;
@@ -197,12 +199,33 @@ static bool BbMixAndDecimate(const me_gnss_signal_t _sMeSid) {
       iSamplesMs = SOFTMACQ_RAW_SPMS / uDecFactor;
       uNcoStep = CirclesToUint32(
           (double)(SOFTMACQ_FC_GLOG1 +
-                   (_sMeSid.sat - GLO_FCN_OFFSET) * SOFTMACQ_GLOG1_FOFF) /
+                   (mesid.sat - GLO_FCN_OFFSET) * SOFTMACQ_GLOG1_FOFF) /
           (double)SOFTMACQ_RAW_FS);
 
       for (k = 0, h = 0, uNco = 0; k < SOFTMACQ_SAMPLE_GRABBER_LENGTH; k++) {
         uSample = ((puSampleBuf[k] >> 2) & 0x3)
                   << BBNCO_CARRPH_BITS; /** B3..2 are Channel 2 */
+        uNcoVal = (uNco >> (32 - BBNCO_CARRPH_BITS)) & BBNCO_CARRPH_MASK;
+
+        h = k / uDecFactor;
+        if (h == SOFTMACQ_BASEBAND_SIZE) break;
+
+        pBaseBand[h].r += bbConvTable[(uSample | uNcoVal)].r;
+        pBaseBand[h].i += bbConvTable[(uSample | uNcoVal)].i;
+        uNco += uNcoStep;
+      }
+      break;
+
+    case CODE_BDS2_B11:
+      uDecFactor = SOFTMACQ_DECFACT_BDS2B1;
+      iSamplesMs = SOFTMACQ_RAW_SPMS / uDecFactor;
+      uNcoStep = CirclesToUint32(
+          (double)(SOFTMACQ_FC_BDS2B1) /
+          (double)SOFTMACQ_RAW_FS);
+
+      for (k = 0, h = 0, uNco = 0; k < SOFTMACQ_SAMPLE_GRABBER_LENGTH; k++) {
+        uSample = ((puSampleBuf[k] >> 0) & 0x3)
+                  << BBNCO_CARRPH_BITS; /** B1..0 are Channel 1 */
         uNcoVal = (uNco >> (32 - BBNCO_CARRPH_BITS)) & BBNCO_CARRPH_MASK;
 
         h = k / uDecFactor;
@@ -225,7 +248,6 @@ static bool BbMixAndDecimate(const me_gnss_signal_t _sMeSid) {
     case CODE_GPS_L5I:
     case CODE_GPS_L5Q:
     case CODE_GPS_L5X:
-    case CODE_BDS2_B11:
     case CODE_BDS2_B2:
     case CODE_GAL_E1B:
     case CODE_GAL_E1C:
