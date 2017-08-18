6f9217ee83eb03a658dfabf06023c73bbb1fa577
Michele Bavaro <michele@swiftnav.com>
2017-08-19 01:14:23 +0300

    waiting for TRK_CODE_INT_MAX changes from FPGA team

--- src/board/v3/nap/track_channel.c
+++ src/board/v3/nap/track_channel.c
@@ -244,14 +244,6 @@ void nap_track_init(u8 channel,
                       round((s->spacing[0].chips + s->spacing[1].chips) *
                             calc_samples_per_chip(chip_rate));
 
-  /* Delay L2CL code phase by 1 chip to accommodate zero in the L2CM slot */
-  /* Initial correlation length for L2CL is thus 1 chip shorter,
-   * since first L2CM chip is skipped */
-  if (mesid.code == CODE_GPS_L2CL) {
-    code_phase += 1.0f;
-    delta_samples -= calc_samples_per_chip(chip_rate);
-  }
-
   /* MIC_COMMENT: nap_track_update_init() so that nap_track_update()
    * does not have to branch for the special "init" situation */
   /* Chip rate */
@@ -306,20 +298,16 @@ void nap_track_init(u8 channel,
   u64 tc_next_rollover;
   double code_samples;
   u8 index = 0;
-  if (mesid.code == CODE_GPS_L2CL) {
-    code_chips = GPS_L2CL_PRN_CHIPS_PER_INTERVAL;
-    code_samples = (double)code_chips * calc_samples_per_chip(chip_rate);
-    num_codes = 1 + (u32)floor((double)samples_diff / code_samples);
-    index = (num_codes % GPS_L2CL_PRN_START_POINTS);
-  } else {
-    code_chips = code_to_chip_count(mesid.code);
-    code_samples = (double)code_chips * calc_samples_per_chip(chip_rate);
-    num_codes = 1 + (u32)floor((double)samples_diff / code_samples);
-  }
+
+  code_chips = code_to_chip_count(mesid.code);
+  code_samples = (double)code_chips * calc_samples_per_chip(chip_rate);
+  num_codes = 1 + (u32)floor((double)samples_diff / code_samples);
+  index = (num_codes % GPS_L2CL_PRN_START_POINTS);
+
   tc_next_rollover =
       tc_codestart + (u64)floor(0.5 + (double)num_codes * code_samples);
 
-  NAP->TRK_CODE_INT_INIT = index * code_chips;
+  NAP->TRK_CODE_INT_INIT = 0 * code_chips;
   NAP->TRK_CODE_FRAC_INIT = 0;
 
   NAP->TRK_CODE_INT_MAX = code_to_chip_count(mesid.code) - 1;
