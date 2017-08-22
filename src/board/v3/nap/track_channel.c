669d89a98342cb690e11a4f0b0dd2286dde1ab5f
Johannes Walter <johannes@swift-nav.com>
2017-08-22 14:37:34 -0700

    NAP: GPS L2 code rollover registers.

--- src/board/v3/nap/track_channel.c
+++ src/board/v3/nap/track_channel.c
@@ -322,10 +322,12 @@ void nap_track_init(u8 channel,
 
   NAP->TRK_CODE_INT_MAX = code_to_chip_count(mesid.code) - 1;
 
-  NAP->TRK_CODE_LFSR0_INIT = mesid_to_init_g1(mesid, index);
-  NAP->TRK_CODE_LFSR0_RESET = mesid_to_init_g1(mesid, 0);
-  NAP->TRK_CODE_LFSR1_INIT = mesid_to_init_g2(mesid);
-  NAP->TRK_CODE_LFSR1_RESET = mesid_to_init_g2(mesid);
+  NAP->TRK_CODE_LFSR0_INIT = mesid_to_lfsr_init(mesid, index);
+  NAP->TRK_CODE_LFSR0_RESET = mesid_to_lfsr_init(mesid, 0);
+
+  NAP->TRK_CODE_LFSR1_INIT = mesid_to_lfsr_init(mesid, 0);
+  NAP->TRK_CODE_LFSR1_RESET = mesid_to_lfsr_init(mesid, 0);
+  NAP->TRK_CODE_LFSR1_LAST = mesid_to_lfsr_last(mesid);
 
   /* port FCN-induced NCO phase to a common receiver clock point */
   s->reckoned_carr_phase = (s->fcn_freq_hz) *
