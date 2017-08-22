93ee9904daf6f1671f8377af54d00aaaf5940796
axlan <feros32@gmail.com>
2017-08-21 17:30:29 -0700

    Added description file Allows PR to be determined from S3 file

---
+++ travis_pr_describe.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright (C) 2016 Swift Navigation Inc.
+# Contact: Jonathan Diamond <jonathan@swiftnav.com>
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Script for publishing built binaries to S3.
+
+set -e
+
+DESCRIPTION_FILE="build_v3_prod/pr_description.txt"
+
+if ! [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  printf "$TRAVIS_PULL_REQUEST\n$TRAVIS_PULL_REQUEST_SHA\n" >> $DESCRIPTION_FILE
+  ./publish.sh $DESCRIPTION_FILE
+fi
