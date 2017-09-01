992deb2519858b640e235754289c037914312605
Adel Mamin <adel.mamin@exafore.com>
2017-09-01 14:39:26 +0300

    Reduce the stabilization timeout for sensitivity profile to 50ms

--- src/track/track_profiles.c
+++ src/track/track_profiles.c
@@ -474,7 +474,7 @@ static const tp_profile_entry_t gnss_track_profiles[] = {
   [IDX_SENS] =
   { {    0,             1,            1,   TP_CTRL_FLL2,             TP_TM_20MS,
                TP_TM_10MS,    TRACK_CN0_EST_SECONDARY },   TP_LD_PARAMS_FLL_5MS,
-      4000,             0,          32.,            1.5,                    32.,
+        50,             0,          32.,            1.5,                    32.,
       IDX_SENS,  IDX_NONE, IDX_TRAN_CN0,   IDX_TRAN_DYN,               IDX_NONE,
       TP_HIGH_CN0_WAIT_PLOCK_N0_DYN | TP_HIGH_DYN_WAIT_PLOCK_CN0 |
       TP_WAIT_CN0 | TP_USE_NEXT },
