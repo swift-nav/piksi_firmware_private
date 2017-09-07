11e59e775b86ffe967c984ebc54e3352cce3b470
Pasi Miettinen <pasi.miettinen@exafore.com>
2017-09-07 13:25:43 +0300

    Fix unit test issues

--- src/piksi_systime.h
+++ src/piksi_systime.h
@@ -17,6 +17,8 @@
 
 #include <libswiftnav/common.h>
 
+#include "chconf.h"
+
 typedef struct piksi_systime {
   systime_t systime;
   u32 rollover_cnt;
@@ -26,6 +28,10 @@ typedef struct piksi_systime {
 #define PIKSI_SYSTIME_SYSTIME_INIT 0
 #define PIKSI_SYSTIME_INIT \
   ((piksi_systime_t){PIKSI_SYSTIME_SYSTIME_INIT, PIKSI_SYSTIME_RO_CNT_INIT})
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 bool piksi_systime_get(piksi_systime_t *t);
 bool piksi_systime_get_x(piksi_systime_t *t);
@@ -48,9 +54,9 @@ bool piksi_systime_dec_s(piksi_systime_t *t, u64 dec);
 
 s8 piksi_systime_cmp(const piksi_systime_t *a, const piksi_systime_t *b);
 
-u64 piksi_systime_sub_us(const piksi_systime_t *a, const piksi_systime_t *b);
-u64 piksi_systime_sub_ms(const piksi_systime_t *a, const piksi_systime_t *b);
-u64 piksi_systime_sub_s(const piksi_systime_t *a, const piksi_systime_t *b);
+s64 piksi_systime_sub_us(const piksi_systime_t *a, const piksi_systime_t *b);
+s64 piksi_systime_sub_ms(const piksi_systime_t *a, const piksi_systime_t *b);
+s64 piksi_systime_sub_s(const piksi_systime_t *a, const piksi_systime_t *b);
 
 void piksi_systime_sleep_us_s(u32 len_us);
 
@@ -63,4 +69,7 @@ u32 piksi_systime_sleep_until_windowed_us(const piksi_systime_t *t,
 u32 piksi_systime_sleep_until_windowed_ms(const piksi_systime_t *t,
                                           u32 window_len_ms);
 
+#ifdef __cplusplus
+}
+#endif
 #endif /* SWIFTNAV_PIKSI_SYSTIME_H */
