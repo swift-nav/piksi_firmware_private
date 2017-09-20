455bf78a74af6f56576c84054daac10b64350790
Pasi Miettinen <pmiettinen@exafore.com>
2017-09-20 13:38:18 +0300

    Piksi systime unit tests RELEASE (#1131)
    * Add piksi_systime unit test framework
    
    * Fix unit test issues
    
    * Clean up stub file
    
    * clang
    
    * More piksi_systime unit tests
    
    * Fix bugs found by piksi_systime unit tests
    
    * Add new unit tests for piksi_systime
    
    * Fix unit test findings
    
    * Review comments
    
    * Travis compatibility issues
    
    * clang
    
    * Travis OS compatibility issues
    
    * Review comments
    
    * Review comments
    
    * Use assert

---
+++ tests/ch.h
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2017 Swift Navigation Inc.
+ * Contact: Pasi Miettinen <pasi.miettinen@exafore.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef _CH_H_
+#define _CH_H_
+
+#define CH_KERNEL_MAJOR 3
+#define CH_KERNEL_MINOR 1
+#define CH_KERNEL_PATCH 3
+
+typedef unsigned int systime_t;
+
+void chSysLock(){};
+void chSysUnlock(){};
+
+systime_t chVTGetSystemTimeX() { return 0; };
+systime_t chThdSleep() { return 1; };
+systime_t chThdSleepS() { return 1; };
+
+#define TIME_INFINITE (systime_t)(-1)
+
+#endif /* _CH_H_ */
+
+/** @} */
