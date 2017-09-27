4204cb8e00b66d99e40b2a7ab86943e8bd4ac6bb
Pasi Miettinen <pasi.miettinen@exafore.com>
2017-09-27 11:05:08 +0300

    pinpoint assert, piksi_systime_inc_internal() only

--- src/piksi_systime.c
+++ src/piksi_systime.c
@@ -127,6 +127,10 @@ static inline u64 s2st(u64 s) {
 static void piksi_systime_get_internal(piksi_systime_t *t) {
   static piksi_systime_t prev = PIKSI_SYSTIME_INIT;
 
+  if (NULL == t) {
+    return;
+  }
+
   assert(t);
 
   systime_t current;
@@ -404,6 +408,9 @@ void piksi_systime_inc_s(piksi_systime_t *t, u64 inc) {
  * \param[in] inc           System tick value to be decreased.
  */
 void piksi_systime_dec_internal(piksi_systime_t *t, u64 dec) {
+  if (NULL == t) {
+    return;
+  }
 
   assert(t);
 
