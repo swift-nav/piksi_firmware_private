d85e95e747e4acee877ba7bf17cd9479a223ad7f
Michele Bavaro <michele@swiftnav.com>
2017-12-13 21:29:12 -0800

    fixed scope for biases, changed carrier phase sign

--- src/observation_biases_calibration.h
+++ src/observation_biases_calibration.h
@@ -18,53 +18,11 @@
 #include "sbp.h"
 #include "timing.h"
 
-/* Empirical corrections for GLO per-frequency pseudorange bias as per
- * https://github.com/swift-nav/piksi_v3_bug_tracking/issues/606#issuecomment-323163617
- * to align observations to Septentrio receiver-type biases
- */
-static const double glo_l1_isc[] = {[0] = -7.25,
-                                    [1] = -7.37,
-                                    [2] = -7.5,
-                                    [3] = -7.57,
-                                    [4] = -7.51,
-                                    [5] = -7.25,
-                                    [6] = -7,
-                                    [7] = -6.72,
-                                    [8] = -7,
-                                    [9] = -7.3,
-                                    [10] = -7.73,
-                                    [11] = -8.45,
-                                    [12] = -8.95,
-                                    [13] = -9.5};
-
-static const double glo_l2_isc[] = {[0] = -7.5,
-                                    [1] = -7.26,
-                                    [2] = -6.83,
-                                    [3] = -6.45,
-                                    [4] = -6.27,
-                                    [5] = -6.16,
-                                    [6] = -6,
-                                    [7] = -5.8,
-                                    [8] = -5.5,
-                                    [9] = -5.35,
-                                    [10] = -5.25,
-                                    [11] = -5.0,
-                                    [12] = -5.0,
-                                    [13] = -5.0};
-
-static const double gps_l2_isc = -1.95;
-
-/* These biases are to align the GLONASS carrier phase to the Septentrio
- * receivers carrier phase These biases are in cycles and are proportional to
- * the frequency number
- * */
-static const double glo_l1_carrier_phase_bias = -0.07 / 8;
-static const double glo_l2_carrier_phase_bias = 0;
+/* Send the glonass bias message every 5 observations message */
+extern const u32 biases_message_freq_setting;
 
 void apply_isc_table(u8 n_channels, navigation_measurement_t *nav_meas[]);
 
-// Send the glonass bias message every 5 observations message
-static const u32 biases_message_freq_setting = 5.0;
 void send_glonass_biases(void);
 
 #endif  // PIKSI_FIRMWARE_PRIVATE_OBSERVATION_BIASES_CALIBRATION_H
