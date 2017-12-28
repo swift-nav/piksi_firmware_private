0b5cf616970eaf7b16233f4fada2cb7feac8e311
Pasi Miettinen <pasi.miettinen@exafore.com>
2017-12-28 12:41:54 +0200

    Extract tracker and tracker cfg

---
+++ src/track/track_cfg.h
@@ -0,0 +1,169 @@
+/*
+ * Copyright (C) 2011-2017 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/*
+ * Main tracking: PLL loop selection
+ */
+
+/* FLL-assisted PLL. FLL is first order and PLL is second order */
+#define tl_pll2_state_t aided_tl_state_fll1_pll2_t
+#define tl_pll2_init aided_tl_fll1_pll2_init
+#define tl_pll2_retune aided_tl_fll1_pll2_retune
+#define tl_pll2_update_fll aided_tl_fll1_pll2_update_fll
+#define tl_pll2_update_dll aided_tl_fll1_pll2_update_dll
+#define tl_pll2_adjust aided_tl_fll1_pll2_adjust
+#define tl_pll2_get_dll_error aided_tl_fll1_pll2_get_dll_error
+#define tl_pll2_discr_update aided_tl_fll1_pll2_discr_update
+#define tl_pll2_get_rates aided_tl_fll1_pll2_get_rates
+
+/*
+ * 3rd order PLL loop selection.
+ * The third order PLL loop selection is mutually exclusive from the three
+ * available implementations:
+ *
+ * TRACK_PLL_MODE3_BL
+ * PLL-assisted DLL. FLL and DLL are second order, PLL is third order
+ * Note: Bilinear transform integrator implementation
+ *
+ * TRACK_PLL_MODE3_BC
+ * PLL-assisted DLL. FLL and DLL are second order, PLL is third order
+ * Note: Boxcar integrator implementation
+ *
+ * TRACK_PLL_MODE3_FLL
+ * FLL-assisted PLL. FLL is second order and PLL is third order
+ * Note: Bilinear transform integrator implementation
+ *
+ */
+#define TRACK_PLL_MODE3_BL 1
+#define TRACK_PLL_MODE3_BC 2
+#define TRACK_PLL_MODE3_FLL 3
+
+#ifndef TRACK_PLL_MODE3
+#define TRACK_PLL_MODE3 TRACK_PLL_MODE3_FLL
+#endif
+
+#if TRACK_PLL_MODE3 == TRACK_PLL_MODE3_BL
+#define tl_pll3_state_t aided_tl_state3_t
+#define tl_pll3_init aided_tl_init3
+#define tl_pll3_retune aided_tl_retune3
+#define tl_pll3_update_fll aided_tl_update_fll3
+#define tl_pll3_update_dll aided_tl_update_dll3
+#define tl_pll3_adjust aided_tl_adjust3
+#define tl_pll3_get_dll_error aided_tl_get_dll_error3
+#define tl_pll3_discr_update aided_tl_discr_update3
+#define tl_pll3_get_rates aided_tl_get_rates3
+#elif TRACK_PLL_MODE3 == TRACK_PLL_MODE3_BC
+#define tl_pll3_state_t aided_tl_state3b_t
+#define tl_pll3_init aided_tl_init3b
+#define tl_pll3_retune aided_tl_retune3b
+#define tl_pll3_update_fll aided_tl_update_fll3b
+#define tl_pll3_update_dll aided_tl_update_dll3b
+#define tl_pll3_adjust aided_tl_adjust3b
+#define tl_pll3_get_dll_error aided_tl_get_dll_error3b
+#define tl_pll3_discr_update aided_tl_discr_update3b
+#define tl_pll3_get_rates aided_tl_get_rates3b
+#elif TRACK_PLL_MODE3 == TRACK_PLL_MODE3_FLL
+#define tl_pll3_state_t aided_tl_state_fll2_pll3_t
+#define tl_pll3_init aided_tl_fll2_pll3_init
+#define tl_pll3_retune aided_tl_fll2_pll3_retune
+#define tl_pll3_update_fll aided_tl_fll2_pll3_update_fll
+#define tl_pll3_update_dll aided_tl_fll2_pll3_update_dll
+#define tl_pll3_adjust aided_tl_fll2_pll3_adjust
+#define tl_pll3_get_dll_error aided_tl_fll2_pll3_get_dll_error
+#define tl_pll3_discr_update aided_tl_fll2_pll3_discr_update
+#define tl_pll3_get_rates aided_tl_fll2_pll3_get_rates
+#else
+#error Unsupported 3rd order PLL Mode
+#endif
+
+/*
+ * Main tracking: FLL loop selection
+ */
+
+/* FLL-assisted DLL. FLL is first order and DLL is second order */
+#define tl_fll1_state_t aided_tl_state_fll1_t
+#define tl_fll1_init aided_tl_fll1_init
+#define tl_fll1_retune aided_tl_fll1_retune
+#define tl_fll1_update_fll aided_tl_fll1_update_fll
+#define tl_fll1_update_dll aided_tl_fll1_update_dll
+#define tl_fll1_adjust aided_tl_fll1_adjust
+#define tl_fll1_get_dll_error aided_tl_fll1_get_dll_error
+#define tl_fll1_discr_update aided_tl_fll1_discr_update
+#define tl_fll1_get_rates aided_tl_fll1_get_rates
+
+/* FLL-assisted DLL. FLL and DLL are both second order */
+#define tl_fll2_state_t aided_tl_state_fll2_t
+#define tl_fll2_init aided_tl_fll2_init
+#define tl_fll2_retune aided_tl_fll2_retune
+#define tl_fll2_update_fll aided_tl_fll2_update_fll
+#define tl_fll2_update_dll aided_tl_fll2_update_dll
+#define tl_fll2_adjust aided_tl_fll2_adjust
+#define tl_fll2_get_dll_error aided_tl_fll2_get_dll_error
+#define tl_fll2_discr_update aided_tl_fll2_discr_update
+#define tl_fll2_get_rates aided_tl_fll2_get_rates
+
+/* Bit synchronization and data decoding */
+#define TPF_BSYNC_SET ((u32)1 << 2)
+#define TPF_BSYNC_ADD ((u32)1 << 3)
+#define TPF_BSYNC_UPD ((u32)1 << 4)
+
+/* C/N0 estimator control */
+#define TPF_CN0_SET ((u32)1 << 5)
+#define TPF_CN0_ADD ((u32)1 << 6)
+#define TPF_CN0_USE ((u32)1 << 7)
+
+/* FLL control */
+#define TPF_FLL_SET ((u32)1 << 8)
+#define TPF_FLL_ADD ((u32)1 << 9)
+#define TPF_FLL_USE ((u32)1 << 11)
+#define TPF_FLL_HALFQ ((u32)1 << 12)
+
+/* correlators control */
+#define TPF_EPL_INV ((u32)1 << 13) /* invert correlators */
+#define TPF_EPL_SET ((u32)1 << 14)
+#define TPF_EPL_ADD ((u32)1 << 15)
+#define TPF_EPL_USE ((u32)1 << 16)
+
+/* False lock detector control */
+#define TPF_ALIAS_SET ((u32)1 << 18)
+#define TPF_ALIAS_ADD ((u32)1 << 19)
+#define TPF_ALIAS_1ST ((u32)1 << 20)
+#define TPF_ALIAS_2ND ((u32)1 << 21)
+
+/* Phase lock detector control */
+#define TPF_PLD_SET ((u32)1 << 22)
+#define TPF_PLD_ADD ((u32)1 << 23)
+#define TPF_PLD_USE ((u32)1 << 24)
+
+#define TRACKING_AZIMUTH_UNKNOWN 400
+#define TRACKING_ELEVATION_UNKNOWN 100 /* Default to above elev. mask */
+/** GPS L1 C/A cross-correlation frequency step [hz] */
+#define L1CA_XCORR_FREQ_STEP 1000.f
+/** GPS L1 C/A CN0 threshold for whitelisting [dB-Hz] */
+#define L1CA_XCORR_WHITELIST_THRESHOLD 40.f
+/** GPS L2 CM CN0 threshold for whitelisting [dB-Hz] */
+#define L2CM_XCORR_WHITELIST_THRESHOLD 27.f
+/** GPS L1 C/A CN0 threshold for suspected xcorr [dB-Hz] */
+#define XCORR_SUSPECT_THRESHOLD -15.f
+/** GPS L1 C/A CN0 threshold for confirmed xcorr [dB-Hz] */
+#define XCORR_CONFIRM_THRESHOLD -20.f
+/** cross-correlation update rate [Hz] */
+#define XCORR_UPDATE_RATE (SECS_MS / GPS_L1CA_BIT_LENGTH_MS)
+/** Carrier phases within tolerance are declared equal. [cycles]
+ *  Stable PLL remains within +-15 degree from correct phase.
+ *  360 * 0.08 ~= 30 degrees
+ */
+#define CARRIER_PHASE_TOLERANCE 0.08f
+/** counter for half-cycle ambiguity resolution */
+#define CARRIER_PHASE_AMBIGUITY_COUNTER 50
+/** handover should occur when code phase is near zero [chips] */
+#define HANDOVER_CODE_PHASE_THRESHOLD 0.5
---
+++ src/track/tracker.h
@@ -0,0 +1,461 @@
+/*
+ * Copyright (C) 2011-2017 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#ifndef SWIFTNAV_TRACKER_H
+#define SWIFTNAV_TRACKER_H
+
+#include "track_cfg.h"
+#include "track_loop/trk_loop_common.h"
+
+#define TP_DLL_PLL_MEAS_DIM 5
+
+/**
+ * Tracking mode enumeration.
+ */
+typedef enum {
+  TP_TM_INITIAL, /**< Initial tracking mode */
+
+  TP_TM_1MS_20MS,   /**< 1 ms */
+  TP_TM_1MS_10MS,   /**< 1 ms */
+  TP_TM_1MS_2MS,    /**< 1 ms */
+  TP_TM_1MS_NH20MS, /**< 1 ms */
+
+  TP_TM_2MS_20MS,   /**< 2 ms */
+  TP_TM_2MS_10MS,   /**< 2 ms */
+  TP_TM_2MS_2MS,    /**< 2 ms */
+  TP_TM_2MS_NH20MS, /**< 2 ms */
+
+  TP_TM_5MS_20MS,   /**< 5 ms */
+  TP_TM_5MS_10MS,   /**< 5 ms */
+  TP_TM_5MS_NH20MS, /**< 5 ms */
+
+  TP_TM_10MS_20MS,   /**< 10 ms */
+  TP_TM_10MS_10MS,   /**< 10 ms */
+  TP_TM_10MS_NH20MS, /**< 10 ms */
+
+  TP_TM_20MS_20MS,  /**< 20 ms */
+  TP_TM_20MS_NH20MS /**< 20 ms */
+} tp_tm_e;
+
+/**
+ * Controller types.
+ */
+typedef enum {
+  TP_CTRL_PLL2, /**< 1st order FLL, 2nd order PLL */
+  TP_CTRL_PLL3, /**< 2nd order FLL, 3rd order PLL */
+  TP_CTRL_FLL1, /**< 1st order FLL */
+  TP_CTRL_FLL2, /**< 2nd order FLL */
+} tp_ctrl_e;
+
+struct profile_vars {
+  u8 index;
+  float pll_bw;
+  float fll_bw;
+};
+
+/**
+ * Tracking loop parameters.
+ */
+typedef struct {
+  float code_bw;      /**< Code tracking noise bandwidth in Hz */
+  float code_zeta;    /**< Code tracking loop damping ratio */
+  float code_k;       /**< Code tracking loop gain coefficient */
+  float carr_to_code; /**< */
+  float carr_bw;      /**< Carrier tracking loop noise bandwidth in Hz */
+  float carr_zeta;    /**< Carrier tracking loop damping ratio */
+  float carr_k;       /**< Carrier tracking loop gain coefficient */
+  float fll_bw;       /**< FLL BW */
+  tp_tm_e mode;       /**< Operation mode */
+  tp_ctrl_e ctrl;     /**< Controller type */
+} tp_loop_params_t;
+
+/**
+ * Lock detector parameters.
+ */
+typedef struct {
+  float k1; /**< LPF coefficient */
+  float k2; /**< I scale factor */
+  u16 lp;   /**< Pessimistic count threshold */
+} tp_lock_detect_params_t;
+
+/**
+ * Lock detector parameters.
+ */
+typedef struct {
+  track_cn0_est_e est;
+
+  float track_cn0_use_thres_dbhz;
+  float track_cn0_drop_thres_dbhz;
+  float track_cn0_ambiguity_thres_dbhz;
+} tp_cn0_params_t;
+
+/**
+ * Tracking profile controller data.
+ *
+ * The controller uses this container for managing profile switching logic.
+ */
+typedef struct {
+  float cn0_offset; /**< C/N0 offset in dB to tune thresholds */
+  float filt_cn0;   /**< C/N0 value for decision logic */
+
+  u32 plock : 1;          /**< Pessimistic phase lock flag */
+  u32 flock : 1;          /**< Pessimistic frequency lock flag */
+  u32 bsync : 1;          /**< Bit sync flag */
+  u32 bsync_sticky : 1;   /**< Bit sync flag */
+  u32 profile_update : 1; /**< Flag if the profile update is required */
+  u32 dll_init : 1;       /**< DLL init required */
+  u32 cn0_est : 2;        /**< C/N0 estimator type */
+  u32 use_alias_detection : 1;
+
+  u16 lock_time_ms;         /**< Profile lock count down timer */
+  struct profile_vars cur;  /**< Current profile variables */
+  struct profile_vars next; /**< Next profile variables */
+  u16 print_time;           /**< Time till next debug print [ms] */
+  u32 time_snapshot_ms;     /**< Time snapshot [ms] */
+  s16 bs_delay_ms;          /**< Bit sync delay [ms] or TP_DELAY_UNKNOWN */
+  s16 plock_delay_ms; /**< Pessimistic lock delay [ms] or TP_DELAY_UNKNOWN */
+
+  tp_loop_params_t loop_params; /**< Tracking loop parameters */
+  /** Phase lock detector parameters */
+  tp_lock_detect_params_t ld_phase_params;
+  /** Freq lock detector parameters */
+  tp_lock_detect_params_t ld_freq_params;
+  tp_cn0_params_t cn0_params;
+
+  const struct tp_profile_entry *profiles; /**< Profiles switching table. */
+} tp_profile_t;
+
+/** \} */
+
+/** Counter type. Value changes every time tracking mode changes. */
+typedef u32 update_count_t;
+
+/** Controller parameters for error sigma computations */
+typedef struct {
+  float fll_bw; /**< FLL controller NBW [Hz].
+                     Single sided noise bandwidth in case of
+                     FLL and FLL-assisted PLL tracking */
+  float pll_bw; /**< PLL controller noise bandwidth [Hz].
+                     Single sided noise bandwidth in case of
+                     PLL and FLL-assisted PLL tracking */
+  float dll_bw; /**< DLL controller noise bandwidth [Hz]. */
+  u8 int_ms;    /**< PLL/FLL controller integration time [ms] */
+} track_ctrl_params_t;
+
+/** Parameters for half-cycle ambiguity resolution */
+typedef struct {
+  s8 polarity; /**< Polarity of the matching carrier phases */
+  bool synced; /**< Flag for indicating half-cycle ambiguity resolution */
+} cp_sync_t;
+
+/** \} */
+
+/** \addtogroup tracking
+ * \{ */
+
+typedef u8 tracker_channel_id_t;
+
+typedef enum {
+  STATE_DISABLED,
+  STATE_ENABLED,
+  STATE_DISABLE_REQUESTED,
+  STATE_DISABLE_WAIT
+} state_t;
+
+/* Bitfield */
+typedef enum {
+  ERROR_FLAG_NONE = 0x00,
+  ERROR_FLAG_MISSED_UPDATE = 0x01,
+  ERROR_FLAG_INTERRUPT_WHILE_DISABLED = 0x02,
+} error_flag_t;
+
+/**
+ * Generic tracking channel information for external use.
+ */
+typedef struct {
+  tracker_channel_id_t id; /**< Channel identifier */
+  me_gnss_signal_t mesid;  /**< ME signal identifier */
+  u16 glo_orbit_slot;      /**< GLO orbital slot */
+  u32 flags;               /**< Tracker flags TRACKER_FLAG_... */
+  s32 tow_ms;              /**< ToW [ms] or TOW_UNKNOWN */
+  s32 tow_residual_ns;     /**< Residual to tow_ms [ns] */
+  float cn0;               /**< C/N0 [dB/Hz] */
+  u64 init_timestamp_ms;   /**< Tracking channel init
+                                timestamp [ms] */
+  u64 update_timestamp_ms; /**< Tracking channel last
+                              update timestamp [ms] */
+  u32 sample_count;        /**< Last measurement sample counter */
+  u16 lock_counter;        /**< Lock state counter */
+  float xcorr_freq;        /**< Cross-correlation doppler [hz] */
+  u16 xcorr_count;         /**< Cross-correlation counter */
+  bool xcorr_wl;           /**< Is signal xcorr whitelisted? */
+} tracking_channel_info_t;
+
+/**
+ * Timing information from tracking channel for external use.
+ */
+typedef struct {
+  u32 cn0_usable_ms;       /**< Time with C/N0 above drop threshold [ms] */
+  u32 cn0_drop_ms;         /**< Time with C/N0 below drop threshold [ms] */
+  u32 ld_pess_locked_ms;   /**< Time in pessimistic lock [ms] */
+  u32 ld_pess_unlocked_ms; /**< Time without pessimistic lock [ms] */
+} tracking_channel_time_info_t;
+
+/** Controller parameters for error sigma computations */
+typedef struct {
+  float fll_bw; /**< FLL controller NBW [Hz].
+                     Single sided noise bandwidth in case of
+                     FLL and FLL-assisted PLL tracking */
+  float pll_bw; /**< PLL controller noise bandwidth [Hz].
+                     Single sided noise bandwidth in case of
+                     PLL and FLL-assisted PLL tracking */
+  float dll_bw; /**< DLL controller noise bandwidth [Hz]. */
+  u8 int_ms;    /**< PLL/FLL controller integration time [ms] */
+} tracking_channel_ctrl_info_t;
+
+/** Tracking channel miscellaneous info */
+typedef struct {
+  struct {
+    double value;         /**< Carrier phase offset value [cycles]. */
+    u64 timestamp_ms;     /**< Carrier phase offset timestamp [ms] */
+  } carrier_phase_offset; /**< Carrier phase offset */
+} tracking_channel_misc_info_t;
+
+/**
+ * Phase and frequencies information
+ */
+typedef struct {
+  double code_phase_chips; /**< The code-phase in chips at `receiver_time`. */
+  double code_phase_rate;  /**< Code phase rate in chips/s. */
+  double carrier_phase;    /**< Carrier phase in cycles. */
+  double carrier_freq;     /**< Carrier frequency in Hz. */
+  double
+      carrier_freq_at_lock; /**< Carrier frequency in Hz at last lock time. */
+  float acceleration;       /**< Acceleration [g] */
+} tracking_channel_freq_info_t;
+
+/**
+ * Public data segment.
+ *
+ * Public data segment belongs to a tracking channel and is locked only for
+ * a quick update or data fetch operations.
+ *
+ * The data is grouped according to functional blocks.
+ */
+typedef struct {
+  /** Generic info for externals */
+  volatile tracking_channel_info_t gen_info;
+  /** Timing info for externals */
+  volatile tracking_channel_time_info_t time_info;
+  /** Frequency info for externals */
+  volatile tracking_channel_freq_info_t freq_info;
+  /** Controller parameters */
+  volatile tracking_channel_ctrl_info_t ctrl_info;
+  /** Miscellaneous parameters */
+  volatile tracking_channel_misc_info_t misc_info;
+} tracker_channel_pub_data_t;
+
+/**
+ * Tracker loop state.
+ */
+typedef struct {
+  union {
+    tl_pll2_state_t pll2; /**< Tracking loop filter state. */
+    tl_pll3_state_t pll3; /**< Tracking loop filter state. */
+    tl_fll1_state_t fll1; /**< Tracking loop filter state. */
+    tl_fll2_state_t fll2; /**< Tracking loop filter state. */
+  };
+  tp_ctrl_e ctrl;
+} tp_tl_state_t;
+
+/**
+ * EPL accumulator structure.
+ */
+typedef struct {
+  union {
+    corr_t
+        epl[TP_DLL_PLL_MEAS_DIM]; /**< E|P|L|VE|VL accumulators as a vector */
+    struct {
+      corr_t early;      /**< Early accumulator */
+      corr_t prompt;     /**< Prompt accumulator */
+      corr_t late;       /**< Late accumulator */
+      corr_t very_early; /**< Very Early accumulator */
+      corr_t very_late;  /**< Very Late accumulator */
+    };
+  };
+} tp_epl_corr_t;
+
+/**
+ * Tracker accumulators.
+ *
+ * Tracker uses different frequencies for the following algorithms:
+ * - DLL and PLL
+ * - C/N0 estimator
+ * - FLL tracker
+ * - Alias (false lock) detector
+ * - Lock detector
+ * - Bit synchronization and message decoding
+ */
+typedef struct {
+  tp_epl_corr_t corr_epl; /**< EPL correlation results for DLL and PLL
+                           *   in correlation period. */
+  tp_epl_corr_t corr_cn0; /**< C/N0 accumulators */
+  corr_t corr_fll;        /**< FLL accumulator */
+  corr_t corr_ad;         /**< False lock (alias) detector accumulator */
+  corr_t corr_ld;         /**< Lock detector accumulator */
+  corr_t corr_bit;        /**< Bit sync accumulator */
+} tp_corr_state_t;
+
+/**
+ * GPS L1 C/A tracker data container type.
+ */
+typedef struct {
+  u16 xcorr_counts[NUM_SATS_GPS]; /**< L1 Cross-correlation interval counters */
+  u16 xcorr_count_l2;             /**< L2 Cross-correlation interval counter */
+  u16 xcorr_whitelist_counts
+      [NUM_SATS_GPS]; /**< L1 whitelist interval counters */
+  bool xcorr_whitelist
+      [NUM_SATS_GPS];      /**< L1 Cross-correlation whitelist status */
+  bool xcorr_whitelist_l2; /**< L2 Cross-correlation whitelist status */
+  u8 xcorr_flag : 1;       /**< Cross-correlation flag */
+} gps_l1ca_tracker_data_t;
+
+/**
+ * GPS L2C tracker data container type.
+ */
+typedef struct {
+  u16 xcorr_count_l1;      /**< L1 Cross-correlation interval count */
+  bool xcorr_whitelist;    /**< Cross-correlation whitelist status */
+  bool xcorr_whitelist_l1; /**< L1 Cross-correlation whitelist status */
+  u8 xcorr_flag : 1;       /**< Cross-correlation flag */
+} gps_l2cm_tracker_data_t;
+
+/** Top-level generic tracker channel. */
+typedef struct {
+  /* This portion of the structure is not cleaned-up at tracker channel start */
+
+  /** State of this channel. */
+  state_t state;
+
+  /** Mutex used to permit atomic reads of channel data. */
+  mutex_t mutex;
+  /** Mutex used to permit atomic updates of public channel data. */
+  mutex_t mutex_pub;
+  /** When tracker is disabled in NAP, all tracker state below
+      cleanup_region_start in this structure is cleaned up.
+      Tracker is not reused immediately. There is time window of
+      about 100ms. If during those 100ms NAP generates an unexpected
+      interrupt, then we disable the tracker channel in NAP again
+      using this parameter. So this parameter should preserve its state
+      over the clean up operation. */
+  u8 nap_channel; /**< Associated NAP channel. */
+
+  /* The data to be cleaned-up at init must be placed below */
+
+  int cleanup_region_start;
+
+  me_gnss_signal_t mesid; /**< Current ME signal being decoded. */
+  u16 glo_orbit_slot;     /**< GLO orbital slot. */
+
+  /** Time at which the channel was disabled. */
+  piksi_systime_t disable_time;
+
+  /** FIFO for navigation message bits. */
+  nav_bit_fifo_t nav_bit_fifo;
+  /** Used to sync time decoded from navigation message
+   * back to tracking channel. */
+  nav_data_sync_t nav_data_sync;
+  /** Time since last nav bit was appended to the nav bit FIFO. */
+  u32 nav_bit_TOW_offset_ms;
+  /** Bit sync state. */
+  bit_sync_t bit_sync;
+  /** Polarity of nav message bits. */
+  s8 bit_polarity;
+  /** Increments when tracking new signal. */
+  u16 lock_counter;
+  /** Set if this channel should output I/Q samples on SBP. */
+  bool output_iq;
+  /** Flags if carrier phase integer offset to be reset. */
+  bool reset_cpo;
+  /** Flags if PRN conformity check failed */
+  bool prn_check_fail;
+  /** Flags if tracker is cross-correlated */
+  bool xcorr_flag;
+
+  update_count_t update_count; /**< Number of ms channel has been running */
+  update_count_t cn0_below_use_thres_count;
+  /**< update_count value when C/N0 was
+       last below the use threshold. */
+  update_count_t cn0_above_drop_thres_count;
+  /**< update_count value when C/N0 was
+       last above the drop threshold. */
+  update_count_t ld_pess_change_count;
+  /**< update_count value when pessimistic
+       phase detector has changed last time. */
+  update_count_t xcorr_change_count;
+  /**< update count value when cross-correlation
+       flag has changed last time */
+  s32 TOW_ms;                /**< TOW in ms. */
+  s32 TOW_ms_prev;           /**< previous TOW in ms. */
+  s32 TOW_residual_ns;       /**< Residual to TOW_ms [ns] */
+  u64 sample_count;          /**< Total num samples channel has tracked for. */
+  double code_phase_prompt;  /**< Prompt code phase in chips. */
+  double code_phase_rate;    /**< Code phase rate in chips/s. */
+  double carrier_phase;      /**< Carrier phase in cycles. */
+  double carrier_phase_prev; /**< Previous carrier phase in cycles. */
+  double carrier_freq;       /**< Carrier frequency Hz. */
+  double carrier_freq_prev;  /**< Carrier frequency Hz. */
+  bool carrier_freq_prev_valid;             /**< carrier_freq_prev is valid. */
+  update_count_t carrier_freq_timestamp_ms; /**< carrier_freq_prev timestamp */
+
+  double carrier_freq_at_lock; /**< Carrier frequency snapshot in the presence
+                                    of PLL/FLL pessimistic locks [Hz]. */
+  float unfiltered_freq_error; /**< Unfiltered frequency error at the FLL
+                                    discriminator output [Hz]. */
+  float cn0;                   /**< Current estimate of C/N0. */
+  u32 flags;                   /**< Tracker flags TRACKER_FLAG_... */
+  float acceleration;          /**< Acceleration [g] */
+  float xcorr_freq;            /**< Doppler for cross-correlation [Hz] */
+  u64 init_timestamp_ms;       /**< Tracking channel init timestamp [ms] */
+  u64 update_timestamp_ms;     /**< Tracking channel last update
+                                    timestamp [ms] */
+  bool updated_once;           /**< Tracker was updated at least once flag. */
+  cp_sync_t cp_sync;           /**< Half-cycle ambiguity resolution */
+  glo_health_t health;         /**< GLO SV health info */
+
+  /** Publicly accessible data */
+  tracker_channel_pub_data_t pub_data;
+
+  tp_profile_t profile; /**< Profile controller state. */
+
+  tp_tl_state_t tl_state;      /**< Tracking loop filter state. */
+  tp_corr_state_t corrs;       /**< Correlations */
+  track_cn0_state_t cn0_est;   /**< C/N0 estimator state. */
+  alias_detect_t alias_detect; /**< Alias lock detector. */
+  lock_detect_t ld_phase;      /**< Phase lock detector state. */
+  lock_detect_t ld_freq;       /**< Frequency lock detector state. */
+  lp1_filter_t xcorr_filter;   /**< Low-pass SV POV doppler filter */
+  tp_tm_e tracking_mode;       /**< Tracking mode */
+  u16 cycle_no : 5;            /**< Cycle index inside current
+                                *   integration mode. */
+  u16 use_alias_detection : 1; /**< Flag for alias detection control */
+  u16 has_next_params : 1;     /**< Flag if stage transition is in
+                                *   progress */
+
+  /* Constellation specific data */
+  union {
+    gps_l1ca_tracker_data_t gps_l1ca;
+    gps_l2cm_tracker_data_t gps_l2cm;
+  };
+} tracker_channel_t;
+
+#endif /* SWIFTNAV_TRACKER_H  */
