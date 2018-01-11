ee9024bcc4688759468b845cb7a62738403dd030
Pasi Miettinen <pasi.miettinen@exafore.com>
2018-01-11 11:42:48 +0200

    Try to update GPG key

--- setup.sh
+++ setup.sh
@@ -157,6 +157,9 @@ function run_all_platforms () {
         log_info "Checking system dependencies for Linux..."
         log_info "Please enter your password for apt-get..."
         log_info "Updating..."
+        sudo rm -f /etc/apt/sources.list.d/mongodb*.list
+        sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+        echo "deb [ arch=amd64 ] http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
         sudo apt-get update
         sudo apt-get install -y curl python-pip
         sudo pip install ansible
