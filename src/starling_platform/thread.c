e44a2f6cf8c631b4373544d3a08fb2c75f7ac798
Kevin Dade <kevin@swiftnav.com>
2018-03-16 17:10:46 -0700

    Thread implementation compiles.
    It still has some significant but inconsequential bugs.

--- src/starling_platform/thread.c
+++ src/starling_platform/thread.c
@@ -11,8 +11,38 @@
 
 #include <starling/platform/thread.h>
 #include <ch.h>
+#include <chthreads.h>
 #include <stdint.h>
+#include <limits.h>
 #include <assert.h>
+
+/**
+ * This implementation is a little bit complicated. Because ChibiOS
+ * places some OS data structures in the working stack area,
+ * we have to be very careful about re-purposing stack areas.
+ * 
+ * Here is an overview of the algorithm used:
+ *
+ * Over the lifetime of the program, each thread is assigned a unique ID.
+ * These are just monotonically increasing unsigned integers. This places
+ * an artificial limit on the number of threads that can be created over 
+ * the course of the programs execution.
+ *
+ * There are a limited number of thread working areas (memory regions), so
+ * this in turn limits the number of concurrent threads we can support. When
+ * a thread is done executing, we want to be able to reclaim the memory region
+ * for reassignment. However, a thread technically isn't dead until all other
+ * threads wishing to "join" with it have had the opportunity to do so.
+ *
+ * To solve this problem, we make use of the monotically increasing unique 
+ * thread IDs. Any attempt to join with a thread ID that is currently active
+ * will block until it completes. Attempts to join older thread IDs (with a
+ * lower ID than any current threads) will immediately succeed. An attempt
+ * to join a future thread is an error.
+ *
+ * Whenever an we try to create a new thread, we will first sweep all possible
+ * thread working areas and reclaim any dead threads.
+ */
 
 #define MAX_NUM_THREADS 2
 
@@ -27,47 +57,98 @@ static MUTEX_DECL(internal_lock);
 THD_WORKING_AREA(wa_thread_1, THREAD_STACK_SIZE);
 THD_WORKING_AREA(wa_thread_2, THREAD_STACK_SIZE);
 
-// Used for internal thread bookkeeping.
-typedef struct thread_info_t {
-  void * const working_area; // Immutable working area pointer.
-  bool in_use;
-  void(*fn)(void*);
-  void *user;
-} thread_info_t ;
+// Some handy constants used for keeping track of thread IDs.
+#define THREAD_ID_UNINITIALIZED 0
+#define THREAD_ID_INITIAL       1
+#define THREAD_ID_MAX           UINT_MAX
 
-// Initialize thread info blocks. Note that the working area pointers are immutable.
-static thread_info_t thread_info_array[MAX_NUM_THREADS] = {
-  {wa_thread_1, false, NULL, NULL},
-  {wa_thread_2, false, NULL, NULL},
+// Monotonically increasing count of threads allocated over the course of the runtime.
+static unsigned int next_thread_id = THREAD_ID_INITIAL;
+
+/**
+ * Used for internal bookkeeping. This implementation relies on the 
+ * thread_id, so reads and writes to this variable must be synchronized.
+ *
+ * TODO(kevin) Note about using in_use and thread_id transactionally.
+ */
+typedef struct working_area_t {
+  void *const ptr;          // Immutable working area pointer.
+  thread_t *chibi_tp;          // Pointer to the underlying Chibi thread struct.
+  bool in_use;               // Indicates if this working area is in use or not.
+  unsigned int thread_id;    // Monotonically increasing unique thread ID.
+} working_area_t;
+
+// Initialize working area info blocks. Note that the working area pointers are immutable.
+static working_area_t working_area_info[MAX_NUM_THREADS] = {
+  {wa_thread_1, NULL, false, THREAD_ID_UNINITIALIZED},
+  {wa_thread_2, NULL, false, THREAD_ID_UNINITIALIZED},
 };
 
 /**
- * We need to wrap the user function and data in order to perform
- * the internal bookkeeping. The argument is assumed to point to
- * one of the static thread info elements.
+ * Look over all of the working areas and reclaim the ones which we believe
+ * to be unavailable, but are actually marked as dead by the OS. Must be
+ * very careful not to write race conditions or deadlock. Because this memory
+ * is technically owned by the OS, must use the system lock as well.
+ *
+ * This is the only place in this code with nested locks. Furthermore the system
+ * lock has narrower scope so there should be no deadlock within this module, and
+ * the system lock is guaranteed to release.
  */
-static THD_FUNCTION(wrap_thread_fn, arg) {
-  thread_info_t *thread_info = (thread_info_t*)arg;
-  // If this function is running, the thread must be "in_use".
-  assert(thread_info->in_use);
-  // Let the user function run.
-  if (thread_info->fn) {
-    thread_info->fn(thread_info->user);
-  }
-  // On completion, "free" the working area. Set the pointers to NULL for debuggin.
+static void reclaim_dead_working_areas(void) {
   chMtxLock(&internal_lock);
-  thread_info->in_use = false;
-  thread_info->fn = NULL;
-  thread_info->user = NULL;
+  chSysLock();
+  for (unsigned int i = 0; i < MAX_NUM_THREADS; ++i) {
+    working_area_t *wa = &working_area_info[i];
+    // Working areas that are marked in_use are candidates for being reclaimed.
+    if (wa->in_use && wa->chibi_tp->p_state == CH_STATE_FINAL) {
+      wa->in_use = false;
+      wa->thread_id = THREAD_ID_UNINITIALIZED;
+    }
+  }
+  chSysUnlock();
   chMtxUnlock(&internal_lock);
-  // TODO(kevin) THIS COULD BE A MAJOR BUG IF CHIBIOS MAKES USE OF THE 
-  // WORKING AREA FOR ITS OWN OS-SPECIFIC DATA STRUCTURES. THE CURRENT ASSUMPTION
-  // IS THAT IT ALLOCATES THREAD STATE IN AN OS-PROTECTED AREA SO THE ONLY
-  // CODE THAT EVER TOUCHES THE WORKING AREA IS THE USER FUNCTION.
-  // TODO(kevin) 3/8/2018
-  // It turns out this assumption is invalid. The ChibiOS thread module makes
-  // use of the lower part of the thread workspace. This implementation is 
-  // 100% incorrect.
+}
+
+/**
+ * Traverse the working areas looking for an available one. Must be
+ * careful to synchronize against other accesses of the is_available
+ * field.
+ */
+static working_area_t *find_available_working_area(void) {
+  working_area_t *wa = NULL;
+  chMtxLock(&internal_lock);
+  for (unsigned int i = 0; i < MAX_NUM_THREADS; ++i) {
+    working_area_t *curr = &working_area_info[i];
+    if (!curr->in_use) {
+      wa = curr;
+      wa->in_use = true;
+      wa->thread_id = next_thread_id++;
+      break;
+    }
+  }
+  chMtxUnlock(&internal_lock);
+  return wa;
+}
+
+/**
+ * Compare the given thread ID to the thread IDs currently active in
+ * the working areas. Note that an ID being active is not the same as
+ * the thread being "alive". This just means that the given thread_id
+ * is currently registered to one of the working areas. This is important
+ * because it means that the ChibiOS Thread struct which lives in the working
+ * area is valid and corresponds to the requested ID.
+ */
+bool is_thread_id_active(unsigned int thread_id) {
+  bool match_found = false;
+  chMtxLock(&internal_lock);
+  for (unsigned int i = 0; i < MAX_NUM_THREADS; ++i) {
+    if (working_area_info[i].thread_id == thread_id) {
+      match_found = true;
+      break;
+    }
+  }
+  chMtxUnlock(&internal_lock);
+  return match_found;
 }
 
 /**
@@ -77,45 +158,48 @@ starling_thread_t* starling_thread_create(unsigned int priority,
                                           void(*fn)(void*),
                                           void* user) 
 {
-  thread_info_t *thread_info = NULL;
-  // Look for a thread that isn't in use.
-  chMtxLock(&internal_lock);
-  for (unsigned int i = 0; i < MAX_NUM_THREADS; ++i) {
-    if (!thread_info_array[i].in_use) {
-      thread_info = &thread_info_array[i]; 
-      thread_info->in_use = true;
-      break;
-    }
-  }
-  chMtxUnlock(&internal_lock);
-  // Once we have safely claimed a thread (or not), we are free
-  // to intialize it.
-  if (thread_info) {
-    thread_info->fn = fn;
-    thread_info->user = user;
-    return (void*)chThdCreateStatic(thread_info->working_area, THREAD_STACK_SIZE, 
-                                    priority, &wrap_thread_fn, thread_info); 
-  } else {
+  if (next_thread_id == THREAD_ID_MAX) {
     return NULL;
   }
+  reclaim_dead_working_areas();
+  working_area_t *wa = find_available_working_area();
+  if (wa) {
+    wa->chibi_tp = chThdCreateStatic(wa->ptr, 
+                                     THREAD_STACK_SIZE,
+                                     priority, fn, user); 
+  }
+  return (starling_thread_t*)wa;
 }
 
 /**
- * Let ChibiOS block until the thread is complete.
+ * Let ChibiOS block until the thread is complete. Attempting to
+ * join a dead or non-existant thread returns immediately.
  */
 void starling_thread_join(starling_thread_t* thd) {
-  (void)chThdWait((thread_t*)thd);
+  assert(thd);
+  working_area_t *wa = (working_area_t*)thd;
+  // TODO(kevin) This is techinically an incorrect implementation.
+  // A thread could become non-current between checking and waiting on it.
+  // This would mean the call to chThdWait has a potentially invalid pointer.
+  if (is_thread_id_active(wa->thread_id)) {
+    chThdWait(wa->chibi_tp);
+  }
 }
 
 /**
- * Tell ChibiOS to signal the target thread should terminate.
- */
+ * Tell ChibiOS to signal the target thread should terminate. Attempting
+ * to terminate a dead or non-existant thread returns immediately. */
 void starling_thread_terminate(starling_thread_t* thd) {
-  chThdTerminate((thread_t*)thd);
+  assert(thd);
+  working_area_t *wa = (working_area_t*)thd;
+  // TODO(kevin) Incorrect for same reason as above.
+  if (is_thread_id_active(wa->thread_id)) {
+    chThdTerminate(wa->chibi_tp);
+  } 
 }
 
 /**
- * Use the ChibiOS yield functionality.
+ * Use the ChibiOS yield functionality to yield the current thread.
  */
 void starling_thread_yield(void) {
   chThdYield();
