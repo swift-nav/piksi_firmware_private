44c02296d78c3ea3a7314d579f80b8decfe9901e
Adel Mamin <adel.mamin@exafore.com>
2018-04-05 17:27:30 +0300

    Fix SBAS TOW propagation bug (#1850)

--- src/track/signals/track_sbas_l1.c
+++ src/track/signals/track_sbas_l1.c
@@ -61,114 +61,14 @@ static void tracker_sbas_l1ca_init(tracker_t *tracker_channel) {
   tp_tracker_init(tracker_channel, &sbas_l1ca_config);
 }
 
-/**
- * Performs ToW caching and propagation.
- *
- * SBAS L1 tracker performs ToW update/propagation only on bit edge. This makes
- * it more robust to propagation errors.
- *
- * \param[in,out  tracker_channel Tracker channel data
- * \param[in]     cycle_flags    Current cycle flags.
- *
- * \return None
- */
-static void update_tow_sbas_l1ca(tracker_t *tracker_channel, u32 cycle_flags) {
-  me_gnss_signal_t mesid = tracker_channel->mesid;
-
-  tp_tow_entry_t tow_entry;
-
-  gnss_signal_t sid = construct_sid(mesid.code, mesid.sat);
-  track_sid_db_load_tow(sid, &tow_entry);
-
-  u64 sample_time_tk = nap_sample_time_to_count(tracker_channel->sample_count);
-
-  bool aligned = false;
-
-  if (0 != (cycle_flags & TPF_BSYNC_UPD) &&
-      tracker_bit_aligned(tracker_channel)) {
-    /* Check current state: do we have bit/ToW alignment */
-    aligned = true;
-  }
-
-  if (TOW_UNKNOWN != tracker_channel->TOW_ms && aligned) {
-    /*
-     * Verify ToW alignment
-     * Current block assumes the bit sync has been reached and current
-     * interval has closed a bit interval. ToW shall be aligned by symbol
-     * duration, which is 2ms for SBAS L1.
-     */
-    u8 tail = tracker_channel->TOW_ms % SBAS_L1CA_SYMBOL_LENGTH_MS;
-    if (0 != tail) {
-      s8 error_ms = tail < (SBAS_L1CA_SYMBOL_LENGTH_MS >> 1)
-                        ? -tail
-                        : SBAS_L1CA_SYMBOL_LENGTH_MS - tail;
-
-      log_error_mesid(mesid,
-                      "[+%" PRIu32
-                      "ms] TOW error detected: "
-                      "error=%" PRId8 "ms old_tow=%" PRId32,
-                      tracker_channel->update_count,
-                      error_ms,
-                      tracker_channel->TOW_ms);
-
-      /* This is rude, but safe. Do not expect it to happen normally. */
-      tracker_flag_drop(tracker_channel, CH_DROP_REASON_OUTLIER);
-    }
-  }
-
-  if (TOW_UNKNOWN == tracker_channel->TOW_ms &&
-      TOW_UNKNOWN != tow_entry.TOW_ms) {
-    /* ToW is not known, but there is a cached value */
-    s32 ToW_ms = TOW_UNKNOWN;
-    double error_ms = 0;
-    u64 time_delta_tk = sample_time_tk - tow_entry.sample_time_tk;
-    u8 ms_align =
-        aligned ? SBAS_L1CA_BIT_LENGTH_MS : SBAS_L1CA_SYMBOL_LENGTH_MS;
-
-    ToW_ms =
-        tp_tow_compute(tow_entry.TOW_ms, time_delta_tk, ms_align, &error_ms);
-
-    if (TOW_UNKNOWN != ToW_ms) {
-      log_debug_mesid(mesid,
-                      "[+%" PRIu32 "ms] Initializing TOW from cache [%" PRIu8
-                      "ms]"
-                      " delta=%.2lfms ToW=%" PRId32 "ms error=%lf",
-                      tracker_channel->update_count,
-                      ms_align,
-                      nap_count_to_ms(time_delta_tk),
-                      ToW_ms,
-                      error_ms);
-      tracker_channel->TOW_ms = ToW_ms;
-      if (tp_tow_is_sane(tracker_channel->TOW_ms)) {
-        tracker_channel->flags |= TRACKER_FLAG_TOW_VALID;
-      } else {
-        log_error_mesid(mesid,
-                        "[+%" PRIu32 "ms] Error TOW propagation %" PRId32,
-                        tracker_channel->update_count,
-                        tracker_channel->TOW_ms);
-        tracker_channel->TOW_ms = TOW_UNKNOWN;
-        tracker_channel->flags &= ~TRACKER_FLAG_TOW_VALID;
-      }
-    }
-  }
-
-  bool confirmed = (0 != (tracker_channel->flags & TRACKER_FLAG_CONFIRMED));
-  if (confirmed && aligned &&
-      (tracker_channel->cn0 >= CN0_TOW_CACHE_THRESHOLD)) {
-    /* Update ToW cache:
-     * - bit edge is reached
-     * - CN0 is OK
-     * - Tracker is confirmed
-     */
-    tow_entry.TOW_ms = tracker_channel->TOW_ms;
-    tow_entry.sample_time_tk = sample_time_tk;
-    track_sid_db_update_tow(sid, &tow_entry);
-  }
-}
-
 static void tracker_sbas_l1ca_update(tracker_t *tracker_channel) {
   u32 cflags = tp_tracker_update(tracker_channel, &sbas_l1ca_config);
 
-  /* SBAS L1-specific ToW manipulation */
-  update_tow_sbas_l1ca(tracker_channel, cflags);
+  bool bit_aligned =
+      ((0 != (cflags & TPF_BSYNC_UPD)) && tracker_bit_aligned(tracker_channel));
+
+  if (!bit_aligned) {
+    return;
+  }
+  tracker_tow_cache(tracker_channel);
 }
