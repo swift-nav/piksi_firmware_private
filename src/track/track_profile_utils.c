469ea5847fd741c52c99d9ce304f7bb6be71cba2
Adel Mamin <adel.mamin@exafore.com>
2018-04-09 22:10:14 +0300

    SBAS FLL always runs at 1ms [RELEASE] (#1867)

--- src/track/track_profile_utils.c
+++ src/track/track_profile_utils.c
@@ -267,15 +267,16 @@ static const state_table_t mode_2ms_2ms = {
   .cn0_ms = 2,
   .lockdet_ms = 2,
   .alias_ms = 0, /* not used as equal to flld_ms */
-  .flld_ms = 2,
-  .flll_ms = 2,
+  .flld_ms = 1,
+  .flll_ms = 1,
   .bit_ms = 2,
   .ent_cnt = 2,
   .entries = {
-    {1, TPF_EPL_SET  | TPF_CN0_SET | TPF_BSYNC_SET | TPF_PLD_SET | TPF_FLL_SET},
-    {1, TPF_EPL_ADD  | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD |
-        TPF_EPL_USE  | TPF_CN0_USE | TPF_BSYNC_UPD | TPF_PLD_USE | TPF_FLL_USE
-                                                                 | TPF_FLL_HALFQ},
+    {1, TPF_EPL_SET  | TPF_CN0_SET | TPF_BSYNC_SET | TPF_PLD_SET | TPF_FLL_SET |
+                                                                   TPF_FLL_USE |
+                                                                   TPF_FLL_HALFQ},
+    {1, TPF_EPL_ADD  | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_SET |
+        TPF_EPL_USE  | TPF_CN0_USE | TPF_BSYNC_UPD | TPF_PLD_USE | TPF_FLL_USE},
   }
 };
 
