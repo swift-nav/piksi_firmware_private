3f0eb8d0f43fad1e1f733c936b5c2d6aed2f489b
Niilo Sirola <niilo.sirola@exafore.com>
2018-04-25 16:33:32 +0300

    re-lint

--- src/ndb/ndb_internal.h
+++ src/ndb/ndb_internal.h
@@ -43,7 +43,7 @@ typedef struct {
 /** Flags to enable NV usage on individual data elements */
 #define NDB_USE_NV_IONO 1
 #define NDB_USE_NV_L2C_CAP 1
-#define NDB_USE_NV_LGF 0
+#define NDB_USE_NV_LGF 1
 #define NDB_USE_NV_ALMANAC 1
 #define NDB_USE_NV_EPHEMERIS 0
 #define NDB_USE_NV_UTC 1
