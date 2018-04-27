d0dabbbf7583c0a123d5ee9f07f39afc91b3b0fa
Michele Bavaro <michele.bavaro@gmail.com>
2018-04-27 11:57:58 -0700

    lint

--- src/utils/bit_sync/bit_sync.c
+++ src/utils/bit_sync/bit_sync.c
@@ -32,11 +32,9 @@
  * NH20 = [0 0 0 0 0  1 0 0 1 1  0 1 0 1 0  0 1 1 1 0]
  * XANS = 0,0,0,0,0, 1,1,0,1,0, 1,1,1,1,1, 0,1,0,0,1
  */
-static const s8 nh20_xans[SYMBOL_LENGTH_NH20_MS] =
-  {+1, +1, +1, +1, +1,
-   -1, -1, +1, -1, +1,
-   -1, -1, -1, -1, -1,
-   +1, -1, +1, +1, -1};
+static const s8 nh20_xans[SYMBOL_LENGTH_NH20_MS] = {+1, +1, +1, +1, +1, -1, -1,
+                                                    +1, -1, +1, -1, -1, -1, -1,
+                                                    -1, +1, -1, +1, +1, -1};
 
 /* Galileo E5aQ transitions array, built per satellite */
 static s8 e5q_xans[NUM_SATS_GAL][GAL_CS100_MS];
@@ -237,7 +235,7 @@ static void histogram_update(bit_sync_t *b,
     for (u8 i = 1; i < SYMBOL_LENGTH_NH20_MS; i++) {
       sum += b->histogram[i] * (nh20_xans[i]);
     }
-    if (sum >= 8*SYMBOL_LENGTH_NH20_MS) {
+    if (sum >= 8 * SYMBOL_LENGTH_NH20_MS) {
       /* We are synchronized! */
       b->bit_phase_ref = b->bit_phase;
     }
