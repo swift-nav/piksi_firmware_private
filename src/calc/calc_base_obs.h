2faaf3b9e8f3f87ab84d44824ca34f358b22cbb7
Kevin Dade <kevin@swiftnav.com>
2018-05-07 08:44:19 -0700

    Move obss_t definition into Starling header. (#1962)
    Starling owns this type as part
    of its API. Ultimately, as the API is refined, expect
    to see more clarity in these input types.

--- src/calc/calc_base_obs.h
+++ src/calc/calc_base_obs.h
@@ -25,28 +25,6 @@
 /** \addtogroup base_obs Base station observation handling
  * \{ */
 
-typedef struct {
-  /** GPS system time of the observation. */
-  gps_time_t tor;
-  /** Approximate base station position.
-   * This may be the position as reported by the base station itself or the
-   * position obtained from doing a single point solution using the base
-   * station observations. */
-  double pos_ecef[3];
-  /** Is the `pos_ecef` field valid? */
-  u8 has_pos;
-  /** The known, surveyed base position. */
-  double known_pos_ecef[3];
-  /** Observation Solution */
-  pvt_engine_result_t soln;
-
-  /** Number of observations in the set. */
-  u8 n;
-  u8 sender_id;
-  /** Set of observations. */
-  navigation_measurement_t nm[MAX_CHANNELS];
-} obss_t;
-
 /** Maximum difference between observation times to consider them matched. */
 #define TIME_MATCH_THRESHOLD 2e-3
 
