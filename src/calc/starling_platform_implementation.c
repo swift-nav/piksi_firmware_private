ec8cd71442de1bc119f320e8f976d9845665c2a5
Kevin Dade <kevin@swiftnav.com>
2018-05-25 13:48:19 -0700

    Add platform implementation file.

---
+++ src/calc/starling_platform_implementation.c
@@ -0,0 +1,60 @@
+/*
+ * Copyright (C) 2018 Swift Navigation Inc.
+ * Contact: Kevin Dade <kevin@swiftnav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "starling_platform.h"
+
+#include <libswiftnav/logging.h>
+
+/**
+ * Figure out which subsystem of the PAL this error came from and
+ * then print an appropriate message.
+ */
+void pal_handle_error(pal_rc_t code) {
+  if (!code) {
+    return;
+  } 
+
+  /**
+  * TODO(kevin) It may make sense to also encode the source of the
+  * error into the actual return codes so that the handle error
+  * function can reconstruct the information.
+  */
+  log_error("Error in Starling PAL implementation: " PRIi32, (int32_t)code);
+}
+
+/**
+ * On the Piksi Multi, all data structures are statically allocated and all
+ * we need to do is register them with the OS.
+ */
+pal_rc_t pal_init(void) {
+
+}
+
+/******************************************************************************
+ * THREAD
+ *****************************************************************************/
+
+/**
+ * For Piksi Multi, we support a maximum of two auxiliary threads.
+ */
+static_assert(STARLING_MAX_NUM_THREADS <= 2);
+
+
+pal_rc_t pal_thread_run(unsigned int priority,
+                        void (*fn)(void *), 
+                        void *context) {
+
+}
+
+
+
+
