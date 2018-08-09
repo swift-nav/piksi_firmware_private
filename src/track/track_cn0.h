5dc29a37b23508901f3f184396560d7315eabd04
Tommi Paakki <tpaakki@exafore.com>
2018-08-09 14:46:22 +0300

    Remove unused cn0 est parameters

--- src/track/track_cn0.h
+++ src/track/track_cn0.h
@@ -40,22 +40,12 @@ typedef struct {
  */
 typedef struct {
   u32 cn0_0 : 8;               /**< Initial C/N0 for bootstrap */
-  u32 flags : 8;               /**< Configuration flags */
   u32 cn0_ms : 6;              /**< C/N0 filter interval in ms */
-  u32 type : 2;                /**< Currently used estimator type */
-  u32 ver : 8;                 /**< Configuration version */
   cn0_est_basic_state_t basic; /**< Basic estimator for Very Early tap in use */
 
   float cn0_raw_dbhz; /**< Last unfiltered CN0 estimation [dB-Hz] */
   u32 weak_signal_ms; /**< Signal is below #THRESH_SENS_DBHZ this long [ms] */
 
-  /* Other supported estimators for testing:
-   * cn0_est_ch_state_t   ch;
-   * cn0_est_nwpr_state_t nwpr;
-   * cn0_est_rscn_state_t rscn;
-   * cn0_est_snv_state_t  snv;
-   * cn0_est_svr_state_t  svr;
-   */
   cn0_filter_t filter; /**< Additional C/N0 filter */
 } track_cn0_state_t;
 
@@ -67,15 +57,9 @@ void track_cn0_params_init(void);
 void track_cn0_init(const me_gnss_signal_t mesid,
                     u8 cn0_ms,
                     track_cn0_state_t *e,
-                    float cn0_0,
-                    u8 flags);
-float track_cn0_update(const me_gnss_signal_t mesid,
-                       track_cn0_state_t *e,
-                       u8 int_ms,
-                       float I,
-                       float Q,
-                       float ve_I,
-                       float ve_Q);
+                    float cn0_0);
+float track_cn0_update(
+    track_cn0_state_t *e, u8 int_ms, float I, float Q, float ve_I, float ve_Q);
 float track_cn0_get_offset(u8 cn0_ms);
 
 #ifdef __cplusplus
