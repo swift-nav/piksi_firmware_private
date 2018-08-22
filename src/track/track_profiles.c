787046751dadb44cbc97937a59629e1775c8f174
Adel Mamin <adel.mamin@exafore.com>
2018-08-22 14:24:10 +0300

    Base station tracking profiles start with FLL based tracking loop

--- src/track/track_profiles.c
+++ src/track/track_profiles.c
@@ -384,7 +384,7 @@ static const tp_profile_entry_t tracker_profiles_base[] = {
 */
 
   [IDX_INIT_0] =
-{ {     10,           7,           20,   TP_CTRL_PLL2,
+  { {   10,             7,           20,   TP_CTRL_PLL3,
         TP_TM_INITIAL,  TP_TM_INITIAL,  TP_TM_INITIAL,  TP_TM_INITIAL,  TP_TM_INITIAL},
         TP_LD_PARAMS_PHASE_INI, TP_LD_PARAMS_FREQ_INI,
        100,             0,            0,
@@ -392,7 +392,7 @@ static const tp_profile_entry_t tracker_profiles_base[] = {
       TP_UNAIDED | TP_WAIT_FLOCK},
 
   [IDX_INIT_1] =
-  { { BW_DYN,      BW_DYN,           20,   TP_CTRL_PLL2,
+  { { BW_DYN,           3,           20,   TP_CTRL_PLL3,
         TP_TM_INITIAL,  TP_TM_INITIAL,  TP_TM_INITIAL,  TP_TM_INITIAL,  TP_TM_INITIAL },
         TP_LD_PARAMS_PHASE_INI, TP_LD_PARAMS_FREQ_INI,
        100,             0,            0,
@@ -400,7 +400,7 @@ static const tp_profile_entry_t tracker_profiles_base[] = {
       TP_WAIT_BSYNC | TP_WAIT_PLOCK | TP_UNAIDED },
 
   [IDX_INIT_2] =
-  { { BW_DYN,      BW_DYN,            5,   TP_CTRL_PLL2,
+  { { BW_DYN,           1,            5,   TP_CTRL_PLL3,
       TP_TM_1MS_20MS,  TP_TM_1MS_10MS,  TP_TM_1MS_2MS,  TP_TM_1MS_NH20MS,  TP_TM_1MS_SC4 },
       TP_LD_PARAMS_PHASE_INI, TP_LD_PARAMS_FREQ_INI,
       100,              0,            0,
@@ -408,7 +408,7 @@ static const tp_profile_entry_t tracker_profiles_base[] = {
       TP_WAIT_PLOCK },
 
   [IDX_2MS] =
-  { { BW_DYN,      BW_DYN,            2,   TP_CTRL_PLL2,
+  { { BW_DYN,           0,            2,   TP_CTRL_PLL2,
       TP_TM_2MS_20MS,  TP_TM_2MS_10MS,  TP_TM_2MS_2MS,  TP_TM_2MS_NH20MS,  TP_TM_2MS_SC4 },
       TP_LD_PARAMS_PHASE_2MS, TP_LD_PARAMS_FREQ_2MS,
         40,             0,            0,
@@ -416,7 +416,7 @@ static const tp_profile_entry_t tracker_profiles_base[] = {
       0},
 
   [IDX_5MS] =
-  { { BW_DYN,      BW_DYN,            1,   TP_CTRL_PLL2,
+  { { BW_DYN,           0,            1,   TP_CTRL_PLL2,
       TP_TM_5MS_20MS,  TP_TM_5MS_10MS,  TP_TM_2MS_2MS,  TP_TM_5MS_NH20MS,  TP_TM_4MS_SC4 },
       TP_LD_PARAMS_PHASE_5MS, TP_LD_PARAMS_FREQ_5MS,
       40,               0,            0,
@@ -424,7 +424,7 @@ static const tp_profile_entry_t tracker_profiles_base[] = {
       0},
 
   [IDX_10MS] =
-  { { BW_DYN,      BW_DYN,            1,   TP_CTRL_PLL2,
+  { { BW_DYN,           0,            1,   TP_CTRL_PLL2,
       TP_TM_10MS_20MS,  TP_TM_10MS_10MS,  TP_TM_2MS_2MS, TP_TM_10MS_NH20MS,  TP_TM_10MS_SC4 },
       TP_LD_PARAMS_PHASE_10MS, TP_LD_PARAMS_FREQ_10MS,
       40,               0,            0,
@@ -432,7 +432,7 @@ static const tp_profile_entry_t tracker_profiles_base[] = {
       0},
 
   [IDX_20MS] =
-  { { BW_DYN,      BW_DYN,           .5,   TP_CTRL_PLL2,
+  { { BW_DYN,           0,           .5,   TP_CTRL_PLL2,
       TP_TM_20MS_20MS,  TP_TM_10MS_10MS,  TP_TM_2MS_2MS,  TP_TM_20MS_NH20MS,  TP_TM_20MS_SC4 },
       TP_LD_PARAMS_PHASE_20MS, TP_LD_PARAMS_FREQ_20MS,
       40,               0,            0,
