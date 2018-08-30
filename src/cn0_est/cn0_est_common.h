1bd2569fb3409bdd0a0b619eb1aa7916e9f15928
Tommi Paakki <tpaakki@exafore.com>
2018-08-30 15:26:18 +0300

    Remove cno smoothing. Use init estimate of Pn

--- src/cn0_est/cn0_est_common.h
+++ src/cn0_est/cn0_est_common.h
@@ -40,12 +40,10 @@ typedef struct {
 } cn0_est_rscn_state_t;
 
 typedef struct {
-  float M2;       /**< Running sum of second order moments. */
-  float M4;       /**< Running sum of fourth order moments. */
-  float Pn;       /**< Running sum of noise power. */
-  float cnt_ms;   /**< Counter for CN0 smoothing in milliseconds. */
-  float cn0_db;   /**< Carrier to noise ratio in dB/Hz. */
-  float cn0_init; /**< Initial carrier to noise ratio in dB/Hz. */
+  float M2;     /**< Running sum of second order moments. */
+  float M4;     /**< Running sum of fourth order moments. */
+  float Pn;     /**< Running sum of noise power. */
+  float cn0_db; /**< Carrier to noise ratio in dB/Hz. */
 } cn0_est_mm_state_t;
 
 typedef struct {
