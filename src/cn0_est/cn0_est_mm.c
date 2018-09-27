79e0fbdd2c9796942ad999c8be3678f15f077cb7
Tommi Paakki <tpaakki@exafore.com>
2018-09-27 11:47:01 +0300

    Tune CN0 filtering

--- src/cn0_est/cn0_est_mm.c
+++ src/cn0_est/cn0_est_mm.c
@@ -22,7 +22,7 @@
 /** Filter coefficient for M2 an M4. */
 #define CN0_MM_ALPHA (0.5f)
 /** Filter coefficient for Pn. */
-#define CN0_MM_PN_ALPHA (0.005f)
+#define CN0_MM_PN_ALPHA (0.01f)
 /** Estimate of noise power Pn. For smoother initial CN0 output. */
 #define CN0_MM_PN_INIT (100000.0f)
 
