6a147c699ac6aaf1456158f5a044919fdefad047
Tommi Paakki <tpaakki@exafore.com>
2018-10-05 13:25:31 +0300

    Use 10ms lock detection in 10ms profiles

--- src/track/track_profile_utils.c
+++ src/track/track_profile_utils.c
@@ -619,7 +619,7 @@ static const state_table_t mode_10ms_sc4 = {
  */
 static const state_table_t mode_10ms_20ms = {
   .cn0_ms = 10,
-  .lockdet_ms = 5,
+  .lockdet_ms = 10,
   .alias_ms = 5, /* not used in this profile as replaced by FLL */
   .flld_ms = 5,
   .fpll_ms = 10,
@@ -631,16 +631,16 @@ static const state_table_t mode_10ms_20ms = {
     {1, TPF_CN0_SET | TPF_EPL_SET | TPF_BSYNC_SET | TPF_PLD_SET | TPF_FLL_SET},
     {1, TPF_CN0_ADD | TPF_EPL_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD},
     {3, TPF_CN0_ADD | TPF_EPL_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD |
-                                                    TPF_PLD_USE | TPF_FLL_USE |
+                                                                  TPF_FLL_USE |
                                                                   TPF_FLL_HALFQ},
 
-    {5, TPF_CN0_ADD | TPF_EPL_ADD | TPF_BSYNC_ADD | TPF_PLD_SET | TPF_FLL_SET |
+    {5, TPF_CN0_ADD | TPF_EPL_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_SET |
         TPF_CN0_USE | TPF_EPL_USE |                 TPF_PLD_USE | TPF_FLL_USE | TPF_FPLL_RUN},
 
     {5, TPF_CN0_SET | TPF_EPL_SET | TPF_BSYNC_ADD | TPF_PLD_SET | TPF_FLL_SET |
-                                                    TPF_PLD_USE | TPF_FLL_USE},
+                                                                  TPF_FLL_USE},
 
-    {5, TPF_CN0_ADD | TPF_EPL_ADD | TPF_BSYNC_ADD | TPF_PLD_SET | TPF_FLL_SET |
+    {5, TPF_CN0_ADD | TPF_EPL_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_SET |
         TPF_CN0_USE | TPF_EPL_USE | TPF_BSYNC_UPD | TPF_PLD_USE | TPF_FLL_USE | TPF_FPLL_RUN}
   }
 };
@@ -650,7 +650,7 @@ static const state_table_t mode_10ms_20ms = {
  */
 static const state_table_t mode_10ms_10ms = {
   .cn0_ms = 10,
-  .lockdet_ms = 5,
+  .lockdet_ms = 10,
   .alias_ms = 2.5,
   .flld_ms = 5,
   .fpll_ms = 10,
@@ -663,10 +663,10 @@ static const state_table_t mode_10ms_10ms = {
     {1, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD |
                                                                                 TPF_ALIAS_1ST},
     {3, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_SET |
-                                                    TPF_PLD_USE | TPF_FLL_USE | TPF_ALIAS_2ND |
+                                                                  TPF_FLL_USE | TPF_ALIAS_2ND |
                                                                   TPF_FLL_HALFQ},
 
-    {2, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_SET | TPF_FLL_SET | TPF_ALIAS_SET |
+    {2, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_SET | TPF_ALIAS_SET |
                                                                                 TPF_ALIAS_2ND},
     {3, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_SET |
         TPF_EPL_USE | TPF_CN0_USE | TPF_BSYNC_UPD | TPF_PLD_USE | TPF_FLL_USE | TPF_ALIAS_2ND | TPF_FPLL_RUN},
@@ -823,7 +823,7 @@ static const state_table_t mode_200ms_10ms = {
  */
 static const state_table_t mode_10ms_nh20ms = {
   .cn0_ms = 10,
-  .lockdet_ms = 5,
+  .lockdet_ms = 10,
   .alias_ms = 5,
   .flld_ms = 10,
   .fpll_ms = 10,
@@ -835,10 +835,10 @@ static const state_table_t mode_10ms_nh20ms = {
     {1, TPF_EPL_SET | TPF_CN0_SET | TPF_BSYNC_SET | TPF_PLD_SET | TPF_FLL_SET | TPF_ALIAS_SET},
     {1, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD},
     {3, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD |
-                                                    TPF_PLD_USE |               TPF_ALIAS_1ST},
+                                                                                TPF_ALIAS_1ST},
 
     {1, TPF_EPL_INV |
-        TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_SET | TPF_FLL_ADD | TPF_ALIAS_ADD},
+        TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD},
     {2, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD},
     {2, TPF_EPL_INV |
         TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD |
@@ -852,9 +852,9 @@ static const state_table_t mode_10ms_nh20ms = {
     {1, TPF_EPL_INV |
         TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD},
     {1, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD |
-                                                    TPF_PLD_USE |               TPF_ALIAS_1ST},
+                                                                                TPF_ALIAS_1ST},
 
-    {1, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_SET | TPF_FLL_ADD | TPF_ALIAS_ADD},
+    {1, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD},
     {3, TPF_EPL_INV |
         TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD},
     {1, TPF_EPL_ADD | TPF_CN0_ADD | TPF_BSYNC_ADD | TPF_PLD_ADD | TPF_FLL_ADD | TPF_ALIAS_ADD |
