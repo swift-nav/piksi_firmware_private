3e8de81e88c4c1a8efa7a5f0db061ae9dc2189b7
Kevin Dade <kevin@swiftnav.com>
2018-10-09 15:06:31 -0700

    Add logs, remove mistaken return statement.

--- src/calc/firmware_starling.c
+++ src/calc/firmware_starling.c
@@ -23,6 +23,7 @@
  * or on Linux. */
 #define STARLING_ON_LINUX_FILENAME "/persistent/flags/starling_on_linux"
 
+
 /**
  * Read from the persistent file to find out if Starling is enabled for Linux
  * or for firmware. If the file exists and contains the character '1', then
@@ -31,7 +32,6 @@
  */
 static bool is_firmware_starling_enabled(void) {
   unsigned char value = '0';
-  return true;
   ssize_t n =
       sbp_fileio_read(STARLING_ON_LINUX_FILENAME, 0, &value, sizeof(value));
   if (n == sizeof(value) && value == '1') {
@@ -47,5 +47,8 @@ void firmware_starling_setup(void) {
   if (!is_firmware_starling_enabled()) {
     /* Inform the bridge that it need not waste time buffering inputs. */
     starling_input_bridge_set_mode(STARLING_BRIDGE_MODE_BYPASS);
+    log_info("Firmware Starling off.");
+  } else {
+    log_info("Firmware Starling on.");
   }
 }
