d7dd42da4c22671cdf82a11f21a1f5aa8c3a3d50
Tommi Paakki <tpaakki@exafore.com>
2018-11-13 10:57:18 +0200

    Use correct lock detector parameters

--- src/track/track_profiles.c
+++ src/track/track_profiles.c
@@ -319,7 +319,7 @@ static const tp_profile_entry_t tracker_profiles_rover[] = {
   [IDX_INIT_2] =
   { { BW_DYN,      BW_DYN,            5,   TP_CTRL_PLL3,
       TP_TM_1MS_20MS,  TP_TM_1MS_10MS,  TP_TM_1MS_2MS,  TP_TM_1MS_NH20MS,  TP_TM_1MS_SC4 },
-      TP_LD_PARAMS_PHASE_INI, TP_LD_PARAMS_FREQ_INI,
+      TP_LD_PARAMS_PHASE_1MS, TP_LD_PARAMS_FREQ_1MS,
       100,             0,            0,
       IDX_NONE, IDX_NONE,     IDX_NONE,
       TP_WAIT_PLOCK },
