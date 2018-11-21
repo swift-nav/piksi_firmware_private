37ac0c24da78ebd5b8212c0d4b02de71815ca436
Niilo Sirola <niilo.sirola@exafore.com>
2018-11-21 19:37:05 +0200

    EX-418: Output also failed measurements (but marked with RAIM flag) (#2471)
    * Send out also failed measurements (marked with RAIM flag)
    
    * Clean up unused code and headers from calc_pvt_common
    
    * Review

--- src/calc/calc_pvt_common.h
+++ src/calc/calc_pvt_common.h
@@ -10,49 +10,16 @@
  * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
  */
 
-#ifndef COMMON_CALC_PVT_H
-#define COMMON_CALC_PVT_H
+#ifndef SWIFTNAV_CALC_PVT_COMMON_H
+#define SWIFTNAV_CALC_PVT_COMMON_H
 
-/* RFT_TODO */
-
-#include <libsbp/navigation.h>
-#include <libsbp/system.h>
-#include <starling/observation.h>
-#include <stdbool.h>
-#include <swiftnav/common.h>
-#include <swiftnav/gnss_time.h>
-#include <swiftnav/single_epoch_solver.h>
-
-#include "acq/manage.h"
-#include "calc_base_obs.h"
-#include "ephemeris/ephemeris.h"
-#include "main/main.h"
-#include "nav_msg/cnav_msg_storage.h"
-#include "nmea/nmea.h"
-#include "peripherals/leds.h"
-#include "position/position.h"
-#include "sbp/sbp.h"
-#include "sbp/sbp_utils.h"
-#include "settings/settings.h"
-#include "shm/shm.h"
-#include "signal_db/signal_db.h"
-#include "simulator/simulator.h"
-#include "system_monitor/system_monitor.h"
-#include "timing/timing.h"
+#include <starling/starling.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 void send_observations(const obs_array_t *obs_array, u32 msg_obs_max_size);
-
-bool gate_covariance(gnss_solution *soln);
-
-void extract_covariance(double full_covariance[9],
-                        double vel_covariance[9],
-                        const gnss_solution *soln);
-
-bool check_covariance(const double pos_accuracy, const double vel_accuracy);
 
 #ifdef __cplusplus
 }
