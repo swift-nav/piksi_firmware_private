7c99ae2647df79f58c8e44ab6b1d417b6794d5f9
Benjamin Altieri <benaltieri@gmail.com>
2018-12-13 23:44:01 -0800

    Revert "do NAP authentication like before (#2557)"
    This reverts commit b19e0995d6a88b6108f79218cbc158e0415017a4.

--- src/main/main.c
+++ src/main/main.c
@@ -64,6 +64,8 @@ int main(void) {
   io_support_init();
   sbp_setup();
 
+  nap_init();
+
   settings_setup();
   timing_setup();
 
@@ -120,9 +122,6 @@ int main(void) {
   static char hw_version_string[16] = {0};
   hw_version_string_get(hw_version_string);
   log_info("hw_version: %s", hw_version_string);
-
-  nap_auth_setup();
-  nap_auth_check();
 
   frontend_setup();
   me_settings_setup();
