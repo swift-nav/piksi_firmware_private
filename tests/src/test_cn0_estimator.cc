736e0e6a1fe7276ba9cdbb0e8c808ae1b65f39b9
Adel Mamin <adel.mamin@exafore.com>
2019-01-18 19:48:25 +0200

    Temporarily remove MM estimator unit tests

--- tests/src/test_cn0_estimator.cc
+++ tests/src/test_cn0_estimator.cc
@@ -109,48 +109,6 @@ TEST(cn0_test, test_cn0_snv) {
   free(signal_Q);
 }
 
-TEST(cn0_test, test_cn0_mm_init) {
-  cn0_est_params_t p;
-  cn0_est_compute_params(&p, CUTOFF_FREQ, LOOP_FREQ, 1, 0);
-  cn0_est_mm_state_t cn0;
-  cn0_est_mm_init(&cn0, 40.f);
-  EXPECT_FLOAT_EQ(cn0.cn0_dbhz, 40.f);
-  EXPECT_FLOAT_EQ(p.log_bw, 30.f);
-  EXPECT_FLOAT_EQ(cn0.M2, -1.0f);
-  EXPECT_FLOAT_EQ(cn0.M4, -1.0f);
-  cn0_est_mm_update(&cn0, CODE_GPS_L1CA, &p, -0.5, 0.f, 0);
-  EXPECT_FLOAT_EQ(cn0.M2, 0.25);
-  EXPECT_FLOAT_EQ(cn0.M4, 0.25f * 0.25f);
-}
-
-TEST(cn0_test, test_cn0_mm) {
-  cn0_est_mm_state_t s;
-  cn0_est_params_t p;
-  s8* signal_I;
-  s8* signal_Q;
-  u32 ii = 0;
-  u32 test_length = 1000;
-  float cn0 = 0.0;
-
-  signal_I = generate_input(test_length, 100);
-  ASSERT_NE((s8*)NULL, signal_I) << "Could not allocate I data";
-  signal_Q = generate_input(test_length, 50);
-  ASSERT_NE((s8*)NULL, signal_Q) << "Could not allocate Q data";
-
-  cn0_est_compute_params(&p, CUTOFF_FREQ, LOOP_FREQ, 1, 0);
-  cn0_est_mm_init(&s, CN0_0);
-
-  for (ii = 0; ii < test_length; ii++) {
-    cn0 =
-        cn0_est_mm_update(&s, CODE_GPS_L1CA, &p, signal_I[ii], signal_Q[ii], 0);
-  }
-
-  EXPECT_GT(cn0, 20.0);
-
-  free(signal_I);
-  free(signal_Q);
-}
-
 TEST(cn0_test, test_cn0_nwpr_init) {
   cn0_est_params_t p;
   cn0_est_compute_params(&p, CUTOFF_FREQ, LOOP_FREQ, 1, 0);
