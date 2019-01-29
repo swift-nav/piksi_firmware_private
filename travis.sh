745f93911c5304f895ea5cf7b94d3dffaa50c1f3
Adel Mamin <adel.mamin@exafore.com>
2019-01-29 11:25:28 +0200

    Make mesta build non-blocking

--- travis.sh
+++ travis.sh
@@ -5,10 +5,11 @@ set -o pipefail
 
 if [ "$TESTENV" == "lint" ]; then
   ./scripts/ci/travis-clang-format-check.sh
+elif [ "$TESTENV" == "mesta" ]; then
+  make -r -C mesta
+  ./mesta/mesta
 else
   export PATH=$PATH:~/gcc-arm-none-eabi/bin;
   make PIKSI_HW=v3 PIKSI_REV=prod -j2;
   make run_tests
-  make -r -C mesta
-  ./mesta/mesta
 fi
