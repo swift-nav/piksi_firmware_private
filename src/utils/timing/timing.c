846fbf9ebbc9a7d9cdf1cb5d68bc332a85608255
Niilo Sirola <niilo.sirola@exafore.com>
2019-02-06 12:42:21 +0200

    Add debug prints

--- src/utils/timing/timing.c
+++ src/utils/timing/timing.c
@@ -144,6 +144,9 @@ void update_time(u64 tc, const gnss_solution *sol) {
     clock_state.P[1][0] = 0.0;
     clock_state.P[1][1] = sol->clock_drift_var;
 
+    /* TODO: GLO double-differences will get biased proportional to FCN unless
+     * this offset is initialized just right, dig to the bottom of this and
+     * simplify */
     gps_time_t ref_time = sol->time;
     double time_subsecond = ref_time.tow - floor(ref_time.tow);
     u64 tc_subsecond = tc % (u64)NAP_FRONTEND_SAMPLE_RATE_Hz;
@@ -437,6 +440,19 @@ double subsecond_cpo_correction(const u64 ref_tc) {
   propagate_clock_state(&clock_state, ref_tc);
 
   double cumulative_offset_s = clock_state.cumulative_offset_s;
+
+  gps_time_t ref_time = napcount2gpstime(ref_tc);
+  double time_subsecond = ref_time.tow - floor(ref_time.tow);
+  u64 tc_subsecond = ref_tc % (u64)NAP_FRONTEND_SAMPLE_RATE_Hz;
+  double cpo_correction = time_subsecond - RX_DT_NOMINAL * tc_subsecond;
+  double cpo_drift = cpo_correction - round(cpo_correction);
+
+  /* TODO: remove debug print */
+  DO_EACH_MS(10000,
+             log_info("cpo_drift %.1f, cum_offs %.1f, diff %.1f",
+                      cpo_drift * 1e9,
+                      -cumulative_offset_s * 1e9,
+                      (cpo_drift + cumulative_offset_s) * 1e9));
 
   return -cumulative_offset_s;
 }
