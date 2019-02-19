17c2f901bb946fcd090dbf75ec36273f20fabe9e
Kevin Dade <kevin@swiftnav.com>
2019-02-19 13:38:19 -0800

    [STAR-440] Further break apart settings code for easy transport to Starling repo. (#2727)
    * Separate settings client for Starling with no dependencies.

--- src/calc/sbp_settings_client.h
+++ src/calc/sbp_settings_client.h
@@ -13,37 +13,10 @@
 #ifndef STARLING_SBP_SETTINGS_CLIENT_H_
 #define STARLING_SBP_SETTINGS_CLIENT_H_
 
-#include <libsbp/sbp.h>
+#include "sbp_duplex_link.h"
+
 #include <libsettings/settings.h>
-
 #include <stddef.h>
-
-// TODO(kevin, jangelo) replace with the SBP refactor interface.
-typedef struct SbpDuplexLink {
-  /* Local sender id. */
-  uint16_t loc_sender_id;
-
-  /* Forward sender id. */
-  uint16_t fwd_sender_id;
-
-  /* Send message using local sender ID. */
-  int (*send)(uint16_t msg_type, uint8_t len, uint8_t *payload);
-
-  /* Send message using given sender ID. */
-  int (*send_from)(uint16_t msg_type,
-                   uint8_t len,
-                   uint8_t *payload,
-                   uint16_t sender);
-
-  /* Register callback on incoming messages. */
-  int (*register_cb)(uint16_t msg_type,
-                     sbp_msg_callback_t cb,
-                     sbp_msg_callbacks_node_t *node,
-                     void *cb_context);
-
-  /* Unregister callback on incoming messages. */
-  int (*unregister_cb)(sbp_msg_callbacks_node_t *node);
-} SbpDuplexLink;
 
 typedef struct SbpSettingsClient SbpSettingsClient;
 
