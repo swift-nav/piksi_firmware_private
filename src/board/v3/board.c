a782c24898cc46d6310c4beb9d523e0b4e5b0457
Gareth McMullin <gareth@blacksphere.co.nz>
2019-03-11 19:05:40 +1300

    Move board rev stuff into board.[ch]

--- src/board/v3/board.c
+++ src/board/v3/board.c
@@ -24,6 +24,13 @@
 #include "sbp.h"
 #include "zynq7000.h"
 
+#define LED_nRST_GPIO_LINE PAL_LINE(GPIO2, 18)
+#define MODEM_PWR_EN_LINE PAL_LINE(GPIO2, 28)
+#define IMU_EN_GPIO_LINE PAL_LINE(GPIO2, 24)
+
+/* NOTE: On Duro, the LED reset is positive logic */
+#define DURO_LED_RST_GPIO_LINE PAL_LINE(GPIO2, 7)
+
 /* Gain for channels not present is char MAX (127 or 0x7f) */
 #define CHAN_NOT_PRESENT_GAIN 0x7f
 
@@ -45,7 +52,30 @@ static void cycle_counter_init(void) {
  * Board-specific initialization code.
  */
 void boardInit(void) {
-  boardRevInit();
+  /* Enable PCAP CLK */
+  *(volatile uint32_t *)0xF8000168 |= (1 << 0);
+
+  palSetLineMode(SPI_SS_IMU_GPIO_LINE, PAL_MODE_OUTPUT);
+  palSetLine(SPI_SS_IMU_GPIO_LINE);
+
+  palSetLineMode(SPI_SS_FRONTEND_GPIO_LINE, PAL_MODE_OUTPUT);
+  palSetLine(SPI_SS_FRONTEND_GPIO_LINE);
+
+  palSetLineMode(SPI_SS_CLK_DAC_GPIO_LINE, PAL_MODE_OUTPUT);
+  palSetLine(SPI_SS_CLK_DAC_GPIO_LINE);
+
+  palSetLineMode(LED_nRST_GPIO_LINE, PAL_MODE_OUTPUT);
+  palSetLine(LED_nRST_GPIO_LINE);
+
+  palSetLineMode(DURO_LED_RST_GPIO_LINE, PAL_MODE_OUTPUT);
+  palClearLine(DURO_LED_RST_GPIO_LINE);
+
+  palSetLineMode(MODEM_PWR_EN_LINE, PAL_MODE_OUTPUT);
+  palSetLine(MODEM_PWR_EN_LINE);
+
+  palSetLineMode(IMU_EN_GPIO_LINE, PAL_MODE_OUTPUT);
+  palSetLine(IMU_EN_GPIO_LINE);
+
   cycle_counter_init();
 }
 
