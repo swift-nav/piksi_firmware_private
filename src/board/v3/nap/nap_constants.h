73f2f44a20e3aae39bb8fe0e3916707781577604
Michael Wurm <mwurm@swiftnav.com>
2019-04-08 14:21:04 -0700

    wip

--- src/board/v3/nap/nap_constants.h
+++ src/board/v3/nap/nap_constants.h
@@ -43,7 +43,7 @@
  * @brief   This is 2 ms expressed as integer number of tracking samples
  * @note    needed for Glonass carrier phase stability
  */
-#define FCN_NCO_RESET_COUNT ((u64)(NAP_FRONTEND_SAMPLE_RATE_Hz / 500))
+#define FCN_NCO_RESET_COUNT ((u64)(NAP_TRACK_SAMPLE_RATE_Hz_GAL / 500))
 
 #define NAP_PPS_TIMING_COUNT_OFFSET (-20)
 #define NAP_EXT_TIMING_COUNT_OFFSET (8)
