da4353d611a334554f7e5e0668a740e44a0dbd37
Ben Altieri <benjaminaltieri@users.noreply.github.com>
2019-05-09 11:50:05 -0700

    [ESD-1316] Gate CAN termination using can_ports flag (#2844)
    * Gate can termination usage on can ports flag
    
    * clang-format
    
    (cherry picked from commit fc49d880fc943fd6eb329531b15e04fec4de252d)

---
+++ src/hal/can_enabled.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2019 Swift Navigation Inc.
+ * Contact: Swift Navigation <dev@swift-nav.com>
+ *
+ * This source is subject to the license found in the file 'LICENSE' which must
+ * be be distributed together with this source. All other rights reserved.
+ *
+ * THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+#include "can_enabled.h"
+
+#include <hal.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "sbp/sbp_fileio.h"
+
+#define CAN_ENABLED_FILENAME "/etc/flags/can_ports"
+#define INS_ACTIVE_FILENAME "/etc/flags/ins_active"
+
+/* If /etc/flags/can_ports exists and is 1, it means can ports are enabled. */
+
+bool can_enabled(void) {
+  unsigned char value = '0';
+  ssize_t n = sbp_fileio_read(CAN_ENABLED_FILENAME, 0, &value, sizeof(value));
+  if (n == sizeof(value) && value == '1') {
+    return true;
+  }
+  return false;
+}
