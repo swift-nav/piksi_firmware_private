4186c658f5b549615b78226b889490ab07a88410
Acer Wang <acer.chi.wang@gmail.com>
2019-07-26 14:39:26 -0700

    fix silly ; typo!

--- src/utils/system_monitor/system_monitor.c
+++ src/utils/system_monitor/system_monitor.c
@@ -110,7 +110,7 @@ static void check_frontend_errors(void) {
     if (frontend_errors) {
       log_error("nt1065: AOK error flag set");
       nt1065_check_plls();
-      nt1065_check_calibration()
+      nt1065_check_calibration();
       nt1065_check_standby();
     }
   }
