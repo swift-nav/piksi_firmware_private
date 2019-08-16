4c7a4a31cfc4a46255bdbd054f9f0bad10fb08a7
Eric Senant <eric.senant@swift-nav.com>
2019-08-16 14:53:56 -0700

    Update scheduler.c
    Revert the reacq skip of -7 to -5 slots

--- src/reacq/scheduler.c
+++ src/reacq/scheduler.c
@@ -97,8 +97,8 @@ static void sch_glo_fcn_set(acq_job_t *job) {
       /* FCN not mapped to GLO slot ID, so perform blind search, just pick next
        * FCN */
       job->mesid.sat++;
-      if (job->mesid.sat > GLO_MAX_FCN || job->mesid.sat < (GLO_MIN_FCN + 3)) {
-        job->mesid.sat = GLO_MIN_FCN + 3;
+      if (job->mesid.sat > GLO_MAX_FCN) {
+        job->mesid.sat = GLO_MIN_FCN;
       } 
       /* now check if the selected frequency already mapped to other slot id */
       if (glo_map_get_slot_id(job->mesid.sat, &slot_id1, &slot_id2) == 0) {
