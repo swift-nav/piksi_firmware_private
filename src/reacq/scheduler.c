8809e39c390e5adb6a116bcc634792a355da78ec
Anthony Cole <anthony@swiftnav.com>
2019-08-27 12:41:47 -0700

    Glo hotfix

--- src/reacq/scheduler.c
+++ src/reacq/scheduler.c
@@ -223,27 +223,13 @@ acq_job_t *sch_select_job(acq_jobs_state_t *jobs_data) {
  * \param job pointer to job to run
  */
 static void sch_glo_fcn_set(acq_job_t *job) {
-  if (NULL == job) {
-    return;
-  }
-
-  u16 slot_id1, slot_id2;
   if (!glo_map_valid(job->sid)) {
-    bool next = true;
-    do {
-      /* FCN not mapped to GLO slot ID, so perform blind search, just pick next
-       * FCN */
-      job->mesid.sat++;
-      if (job->mesid.sat > GLO_MAX_FCN) {
-        job->mesid.sat = GLO_MIN_FCN;
-      }
-      /* now check if the selected frequency already mapped to other slot id */
-      if (glo_map_get_slot_id(job->mesid.sat, &slot_id1, &slot_id2) == 0) {
-        /* selected frequency is not mapped to other slot id, so use it for
-         * acquisition */
-        next = false;
-      }
-    } while (next);
+    /* FCN not mapped to GLO slot ID, so perform blind search, just pick next
+     * FCN */
+    job->mesid.sat++;
+    if (job->mesid.sat > GLO_MAX_FCN) {
+      job->mesid.sat = GLO_MIN_FCN;
+    }
     job->mesid.code = job->sid.code;
   }
 }
