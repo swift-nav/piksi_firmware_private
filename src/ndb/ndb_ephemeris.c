6e536621ab98eda63cff8c5639b38929f9860ee4
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2020-07-21 20:25:06 +1000

    updated ephemeris interfaces

--- src/ndb/ndb_ephemeris.c
+++ src/ndb/ndb_ephemeris.c
@@ -233,7 +233,7 @@ static bool ndb_can_confirm_ephemeris(const ephemeris_t *new,
       if (0 ==
               calc_sat_state_almanac(existing_a, &t, alm_sat_pos, _, _, _, _) &&
           0 == calc_sat_state_n(
-                   new, &t, eph_sat_pos, _, _, _, _, &iodc, &iode)) {
+                   new, &t, MEO, eph_sat_pos, _, _, _, _, &iodc, &iode)) {
         /* Compute distance [m] */
         double d = vector_distance(3, alm_sat_pos, eph_sat_pos);
 
@@ -269,10 +269,11 @@ static bool ndb_can_confirm_ephemeris(const ephemeris_t *new,
 
       ok = false;
 
-      if (0 == calc_sat_state_n(
-                   existing_e, &t, old_sat_pos, _, _, _, _, &iodc, &iode) &&
+      if (0 ==
+              calc_sat_state_n(
+                  existing_e, &t, MEO, old_sat_pos, _, _, _, _, &iodc, &iode) &&
           0 == calc_sat_state_n(
-                   new, &t, new_sat_pos, _, _, _, _, &iodc, &iode)) {
+                   new, &t, MEO, new_sat_pos, _, _, _, _, &iodc, &iode)) {
         /* Compute distance [m] */
         double d = vector_distance(3, old_sat_pos, new_sat_pos);
 
