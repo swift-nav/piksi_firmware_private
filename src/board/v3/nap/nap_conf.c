e195a1399e233e049a45738023de9f5ec3db75e9
Sam Lewis <sam.vr.lewis@gmail.com>
2021-04-06 18:07:38 +1000

    Remove log call from critical section [SV4-63]
    Removes a log call from within the nap_unlock critical section. The log
    implementation uses ChibiOS API calls which should be not used after a
    chSysLock critical section is entered, or else Chibi will invoke
    chSysHalt and the firmware will halt .

--- src/board/v3/nap/nap_conf.c
+++ src/board/v3/nap/nap_conf.c
@@ -124,14 +124,11 @@ void nap_unlock(const u8 key[]) {
    * 'random' time.
    * In the case Linux was interrupting and working with the register, the
    * RSA core would assert the status-busy flag.
-   * This polling would then at least give us information that Linux
-   * interfered here.
    */
   chSysLock();
-  volatile u16 n = 0;
-  while (GET_NAP_STATUS_AUTH_BUSY(NAP->STATUS)) {
-    log_error("Linux interrupted writing of NAP->AUTHENTICATE. Count=%i", n);
-  }
+  while (GET_NAP_STATUS_AUTH_BUSY(NAP->STATUS))
+    ;
+
   for (u32 i = 0; i < NAP_KEY_LENGTH; ++i) {
     NAP->AUTHENTICATION = SET_NAP_AUTHENTICATION_OPERATION(0, 0) |
                           SET_NAP_AUTHENTICATION_ADDR(0, i) |
