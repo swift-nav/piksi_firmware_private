7c735ad08ff8711e3a150ed9959bb600c60fb248
Rodrigo Reichert <rodrigo.reichert@swift-nav.com>
2021-08-23 14:35:25 +1000

    updated rename

--- src/calc/calc_nav_meas.c
+++ src/calc/calc_nav_meas.c
@@ -13,7 +13,7 @@
 #include "calc_nav_meas.h"
 
 #include <assert.h>
-#include <pvt_driver/observations.h>
+#include <pvt_common/observations.h>
 #include <pvt_engine/cycle_slip.h>
 #include <string.h>
 #include <swiftnav/coord_system.h>
--- src/simulator/simulator.h
+++ src/simulator/simulator.h
@@ -14,7 +14,7 @@
 #define SWIFTNAV_SIMULATOR_H
 
 #include <libsbp/tracking.h>
-#include <pvt_driver/observations.h>
+#include <pvt_common/observations.h>
 #include <stdbool.h>
 #include <swiftnav/common.h>
 #include <swiftnav/single_epoch_solver.h>
